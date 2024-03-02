38f1be3023d3974bed6ea8fc6595d32ad8bb1eb3
GitHub Action <action@github.com>
2024-03-02 12:11:13 +0000

    Automated update

--- Casks/ytdownloader.rb
+++ Casks/ytdownloader.rb
@@ -1,11 +1,11 @@
 cask "ytdownloader" do
-  version "v3.17.2"
+  version "v3.17.3"
 
-  url "https://github.com/aandrew-me/ytDownloader/releases/download/v3.17.2/YTDownloader_Mac.dmg"
+  url "https://github.com/aandrew-me/ytDownloader/releases/download/v3.17.3/YTDownloader_Mac.dmg"
   name "ytDownloader"
   desc "A modern GUI App for downloading Videos and Audios from hundreds of sites"
   homepage "https://github.com/aandrew-me/ytDownloader"
-  sha256 "c814560302ca6f31383b87e3384d5a44ffcd546df3b7c9552d2f0c66f62387ee"
+  sha256 "fca04205342897b7014a75ad83513b9f5a1da632165634b03cb66bab6dcf391e"
 
   auto_updates true
 
