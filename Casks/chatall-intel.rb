3fce12a9385f16aa6154c7f23c23d881dbe18484
GitHub Action <action@github.com>
2024-03-17 01:13:02 +0000

    Automated update

--- Casks/chatall-intel.rb
+++ Casks/chatall-intel.rb
@@ -1,11 +1,11 @@
 cask "chatall-intel" do
-  version "v1.61.93"
+  version "v1.62.94"
 
-  url "https://github.com/sunner/ChatALL/releases/download/v1.61.93/ChatALL-1.61.93-mac-x64.dmg"
+  url "https://github.com/sunner/ChatALL/releases/download/v1.62.94/ChatALL-1.62.94-mac-x64.dmg"
   name "ChatALL-intel"
   desc "Concurrently chat with ChatGPT, Bing Chat, bard, Alpaca, Vincuna, Claude, ChatGLM, MOSS, iFlytek Spark, ERNIE and more, discover the best answers"
   homepage "https://github.com/sunner/ChatALL"
-  sha256 "93c31967f642d384900acae0bd05a6ed8bd82e8ce3e3461aebdb88b1ae09d79a"
+  sha256 "3e5a5ba5ae2f46851ff5bddc2ec0b737796824c2c30f4e2cca7f2d3d580855ab"
 
   auto_updates true
 
--- Casks/encrypt0r-arm.rb
+++ Casks/encrypt0r-arm.rb
@@ -1,11 +1,11 @@
 cask "encrypt0r-arm" do
-  version "v3.12.16"
+  version "v3.12.17"
 
-  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.16/encrypt0r-mac-m1.zip"
+  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.17/encrypt0r-mac-m1.zip"
   name "encrypt0r-arm"
   desc "encrypt0r App to encrypt and decrypt your files with a passphrase"
   homepage "https://github.com/kunalnagar/encrypt0r"
-  sha256 "2d1fb775519a71b9ee095d70801b1aa2be9dcf93aa057db20c6e2df1ba928d8a"
+  sha256 "7fa61d02dde2d53548ca2e036e490ac3e55aa199373d6e78aaf95ba6abc6da06"
 
   auto_updates true
 
