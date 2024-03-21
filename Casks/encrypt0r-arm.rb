f417bb74b049d9996dde778012c358d1546252eb
GitHub Action <action@github.com>
2024-03-21 01:11:55 +0000

    Automated update

--- Casks/encrypt0r-arm.rb
+++ Casks/encrypt0r-arm.rb
@@ -1,11 +1,11 @@
 cask "encrypt0r-arm" do
-  version "v3.12.24"
+  version "v3.12.26"
 
-  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.24/encrypt0r-mac-m1.zip"
+  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.26/encrypt0r-mac-m1.zip"
   name "encrypt0r-arm"
   desc "encrypt0r App to encrypt and decrypt your files with a passphrase"
   homepage "https://github.com/kunalnagar/encrypt0r"
-  sha256 "bd04e9956bbf9a0dc5a29c77a90cf847347a2d3ef335d2af9e147c1e83a4c0b2"
+  sha256 "a7d1b1eb68e94a1078a4f99badd5ce8c7b3cc5bc46e7e4a3b89e281f71ddcc95"
 
   auto_updates true
 
