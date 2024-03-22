2928a66871d7826fb46ad3c6eb5271c077de7417
GitHub Action <action@github.com>
2024-03-22 01:08:22 +0000

    Automated update

--- Casks/encrypt0r-arm.rb
+++ Casks/encrypt0r-arm.rb
@@ -1,11 +1,11 @@
 cask "encrypt0r-arm" do
-  version "v3.12.26"
+  version "v3.12.27"
 
-  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.26/encrypt0r-mac-m1.zip"
+  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.27/encrypt0r-mac-m1.zip"
   name "encrypt0r-arm"
   desc "encrypt0r App to encrypt and decrypt your files with a passphrase"
   homepage "https://github.com/kunalnagar/encrypt0r"
-  sha256 "a7d1b1eb68e94a1078a4f99badd5ce8c7b3cc5bc46e7e4a3b89e281f71ddcc95"
+  sha256 "9eeaf1f605696f2b5e7eec95ff755c97c1bc0a134a647144e0606fd10a3c70e5"
 
   auto_updates true
 
--- Casks/halloy.rb
+++ Casks/halloy.rb
@@ -1,11 +1,11 @@
 cask "halloy" do
-  version "2024.4"
+  version "2024.5"
 
-  url "https://github.com/squidowl/halloy/releases/download/2024.4/halloy.dmg"
+  url "https://github.com/squidowl/halloy/releases/download/2024.5/halloy.dmg"
   name "halloy"
   desc "IRC application written in Rust"
   homepage "https://github.com/squidowl/halloy"
-  sha256 "ae30a64353155091a971a5654f1d51f54887088b771e401715f321436388ffe9"
+  sha256 "7ac1bd3b66e89c40d3a8309f57e1b3321e8f1aa59527fcc89541e5767d75628e"
 
   auto_updates true
 
