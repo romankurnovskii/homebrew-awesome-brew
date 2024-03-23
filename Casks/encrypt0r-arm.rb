da4af0462eacbc8c89c75f7b336169be65dcfe51
GitHub Action <action@github.com>
2024-03-23 01:07:15 +0000

    Automated update

--- Casks/encrypt0r-arm.rb
+++ Casks/encrypt0r-arm.rb
@@ -1,11 +1,11 @@
 cask "encrypt0r-arm" do
-  version "v3.12.27"
+  version "v3.12.28"
 
-  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.27/encrypt0r-mac-m1.zip"
+  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.28/encrypt0r-mac-m1.zip"
   name "encrypt0r-arm"
   desc "encrypt0r App to encrypt and decrypt your files with a passphrase"
   homepage "https://github.com/kunalnagar/encrypt0r"
-  sha256 "9eeaf1f605696f2b5e7eec95ff755c97c1bc0a134a647144e0606fd10a3c70e5"
+  sha256 "ca94583543caceb49c2de2af31ae4fe45f34824d0cb6a06933a3002ae5e3d962"
 
   auto_updates true
 
--- Casks/encrypt0r-intel.rb
+++ Casks/encrypt0r-intel.rb
@@ -1,11 +1,11 @@
 cask "encrypt0r-intel" do
-  version "v3.12.27"
+  version "v3.12.28"
 
-  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.27/encrypt0r-mac.zip"
+  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.28/encrypt0r-mac.zip"
   name "encrypt0r-intel"
   desc "encrypt0r App to encrypt and decrypt your files with a passphrase"
   homepage "https://github.com/kunalnagar/encrypt0r"
-  sha256 "508919e3ef7e71496ec9b1eee0ab1cdac3cb86d4e01ab590fa55b236193aae8d"
+  sha256 "796ab729bc4623e9d65ca0c008653606e57d10f40b3c0c7ebf79741cf203bcac"
 
   auto_updates true
 
