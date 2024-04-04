69638ec87853ece199415017aea2f1074b852d7e
GitHub Action <action@github.com>
2024-04-04 01:12:01 +0000

    Automated update

--- Casks/encrypt0r-intel.rb
+++ Casks/encrypt0r-intel.rb
@@ -1,11 +1,11 @@
 cask "encrypt0r-intel" do
-  version "v3.12.39"
+  version "v3.12.40"
 
-  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.39/encrypt0r-mac.zip"
+  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.40/encrypt0r-mac.zip"
   name "encrypt0r-intel"
   desc "encrypt0r App to encrypt and decrypt your files with a passphrase"
   homepage "https://github.com/kunalnagar/encrypt0r"
-  sha256 "9d1615c14361217a2983fe0e67fa32304d5a0c017106efb59fe21d83eca3ed7e"
+  sha256 "f89a34e72c943e8a817ff762e8632784ce28d8db11cf01b49607c2c4a59f9eeb"
 
   auto_updates true
 
--- Casks/zui.rb
+++ Casks/zui.rb
@@ -1,11 +1,11 @@
 cask "zui" do
-  version "v1.6.0"
+  version "v1.7.0"
 
-  url "https://github.com/brimdata/zui/releases/download/v1.6.0/Zui-1.6.0-mac.zip"
+  url "https://github.com/brimdata/zui/releases/download/v1.7.0/Zui-1.7.0-mac.zip"
   name "zui"
   desc "Zui is a powerful desktop application for exploring and working with data. The official front-end to the Zed lake."
   homepage "https://github.com/brimdata/zui"
-  sha256 "3781ac0c123722a3db0623346677e4e800814e80081dacb7c3cf4b4dcca4a460"
+  sha256 "e9424aaabb4b76afe1428e0f8ee5faac9f108ce312792f0a68e49bd5684de358"
 
   auto_updates true
 
