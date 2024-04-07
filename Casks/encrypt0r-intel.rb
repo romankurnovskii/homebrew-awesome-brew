c32872e3468e43d281d444afeee95a2723728f11
GitHub Action <action@github.com>
2024-04-07 01:15:22 +0000

    Automated update

--- Casks/encrypt0r-intel.rb
+++ Casks/encrypt0r-intel.rb
@@ -1,11 +1,11 @@
 cask "encrypt0r-intel" do
-  version "v3.12.42"
+  version "v3.12.43"
 
-  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.42/encrypt0r-mac.zip"
+  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.43/encrypt0r-mac.zip"
   name "encrypt0r-intel"
   desc "encrypt0r App to encrypt and decrypt your files with a passphrase"
   homepage "https://github.com/kunalnagar/encrypt0r"
-  sha256 "26ed59c69bf262fd5e07990fc4eac4062586e0eea027236fddbd2d4a4204cb7d"
+  sha256 "e2a24cd3b3e4f5ca81e2379f1df920f8d4a91b0289b7160ce47c0e06029bf8d8"
 
   auto_updates true
 
