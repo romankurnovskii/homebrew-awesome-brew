20f96d24f242a38a7ea7c9d3e4cdd9f543fe91ba
GitHub Action <action@github.com>
2024-04-13 00:58:44 +0000

    Automated update

--- Casks/super-productivity.rb
+++ Casks/super-productivity.rb
@@ -1,11 +1,11 @@
 cask "super-productivity" do
-  version "v8.0.1"
+  version "v8.0.5"
 
-  url "https://github.com/johannesjo/super-productivity/releases/download/v8.0.1/superProductivity-8.0.1-mac.zip"
+  url "https://github.com/johannesjo/super-productivity/releases/download/v8.0.5/superProductivity-8.0.5-mac.zip"
   name "super-productivity"
   desc "Super Productivity is an advanced todo list app with integrated Timeboxing and time tracking capabilities. It also comes with integrations for Jira, Gitlab, GitHub and Open Project."
   homepage "https://github.com/johannesjo/super-productivity"
-  sha256 "cba6b7b9304fbd3e0f70beac65bb9a649a363ee6c41b58b1a70acf5442ed245a"
+  sha256 "174e63ccd96147d5ed5d4cf33eb80388953b715132bb026d15765c60c7f86fcc"
 
   auto_updates true
 
