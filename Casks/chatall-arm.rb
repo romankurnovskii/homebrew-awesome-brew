48251df52dd0d706b5ef0525ed763146770d2301
GitHub Action <action@github.com>
2024-04-15 03:29:22 +0000

    Automated update

--- Casks/chatall-arm.rb
+++ Casks/chatall-arm.rb
@@ -1,11 +1,11 @@
 cask "chatall-arm" do
-  version "v1.69.99"
+  version "v1.71.100"
 
-  url "https://github.com/sunner/ChatALL/releases/download/v1.69.99/ChatALL-1.69.99-mac-arm64.dmg"
+  url "https://github.com/sunner/ChatALL/releases/download/v1.71.100/ChatALL-1.71.100-mac-arm64.dmg"
   name "ChatALL-arm"
   desc "Concurrently chat with ChatGPT, Bing Chat, bard, Alpaca, Vincuna, Claude, ChatGLM, MOSS, iFlytek Spark, ERNIE and more, discover the best answers"
   homepage "https://github.com/sunner/ChatALL"
-  sha256 "eac056e6e9ee5de9f5bf0c6ff289fdc478a578b235cdf5c57b8cc20f97085ad4"
+  sha256 "1812d7467ea06f039f9b20287faecf5a5374d8997ca49a12fa5f6594514562b1"
 
   auto_updates true
 
--- Casks/desktop.rb
+++ Casks/desktop.rb
@@ -1,11 +1,11 @@
 cask "desktop" do
-  version "v1.11.0"
+  version "v1.11.1"
 
-  url "https://github.com/TurboWarp/desktop/releases/download/v1.11.0/TurboWarp-Setup-1.11.0.dmg"
+  url "https://github.com/TurboWarp/desktop/releases/download/v1.11.1/TurboWarp-Setup-1.11.1.dmg"
   name "desktop"
   desc "Offline editor for Scratch 3"
   homepage "https://github.com/TurboWarp/desktop"
-  sha256 "b5b9c45d4d2611d7e0677cbfd1dd42f5f34aa35e254185d570851b7f7d2b6cae"
+  sha256 "efd8bed5e711e213500b3c1f438edf7e6e5be35f2acd2af38a217e3ce9f793c0"
 
   auto_updates true
 
