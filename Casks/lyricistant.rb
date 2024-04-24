11b3f663c0040939d9ef8dbd22ad62acdac01bd8
GitHub Action <action@github.com>
2024-04-24 01:14:48 +0000

    Automated update

--- Casks/lyricistant.rb
+++ Casks/lyricistant.rb
@@ -1,11 +1,11 @@
 cask "lyricistant" do
-  version "v3.4.2"
+  version "v3.4.3"
 
-  url "https://github.com/wardellbagby/lyricistant/releases/download/v3.4.2/lyricistant-mac_x64.dmg"
+  url "https://github.com/wardellbagby/lyricistant/releases/download/v3.4.3/lyricistant-mac_x64.dmg"
   name "lyricistant"
   desc "A helpful writing assistant for lyricists!"
   homepage "https://github.com/wardellbagby/lyricistant"
-  sha256 "b5b9e8a74c2aaf0e5de5ff34427744bab39ea1e7f12a470ae127560f28c0b7cf"
+  sha256 "de405797f233446d2bbcd2d0df9f1ddc294773e842865b282a2bdc54ad36680f"
 
   auto_updates true
 
