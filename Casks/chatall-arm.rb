40d2d088d559e3ad27ad05a773a704b87002d3ee
GitHub Action <action@github.com>
2024-05-02 01:11:07 +0000

    Automated update

--- Casks/chatall-arm.rb
+++ Casks/chatall-arm.rb
@@ -1,11 +1,11 @@
 cask "chatall-arm" do
-  version "v1.77.102"
+  version "v1.78.103"
 
-  url "https://github.com/sunner/ChatALL/releases/download/v1.77.102/ChatALL-1.77.102-mac-arm64.dmg"
+  url "https://github.com/sunner/ChatALL/releases/download/v1.78.103/ChatALL-1.78.103-mac-arm64.dmg"
   name "ChatALL-arm"
   desc "Concurrently chat with ChatGPT, Bing Chat, bard, Alpaca, Vincuna, Claude, ChatGLM, MOSS, iFlytek Spark, ERNIE and more, discover the best answers"
   homepage "https://github.com/sunner/ChatALL"
-  sha256 "fecb88f685d242fc650cbcefa8ddd3c67f00a9d8d6c159e9c79d197ba2a65a1e"
+  sha256 "c9aaec64fc01410539dd315aa8742ba99edfc3afe36c5957d7303d3a3979393a"
 
   auto_updates true
 
