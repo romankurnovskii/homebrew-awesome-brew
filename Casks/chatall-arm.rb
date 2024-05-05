8dca15973231732533f8c95b2745c118d2791226
GitHub Action <action@github.com>
2024-05-05 01:16:26 +0000

    Automated update

--- Casks/chatall-arm.rb
+++ Casks/chatall-arm.rb
@@ -1,11 +1,11 @@
 cask "chatall-arm" do
-  version "v1.78.103"
+  version "v1.78.104"
 
-  url "https://github.com/sunner/ChatALL/releases/download/v1.78.103/ChatALL-1.78.103-mac-arm64.dmg"
+  url "https://github.com/sunner/ChatALL/releases/download/v1.78.104/ChatALL-1.78.104-mac-arm64.dmg"
   name "ChatALL-arm"
   desc "Concurrently chat with ChatGPT, Bing Chat, bard, Alpaca, Vincuna, Claude, ChatGLM, MOSS, iFlytek Spark, ERNIE and more, discover the best answers"
   homepage "https://github.com/sunner/ChatALL"
-  sha256 "c9aaec64fc01410539dd315aa8742ba99edfc3afe36c5957d7303d3a3979393a"
+  sha256 "20f064958096f24c53aaf1f872d7805fcc504d6f8dc7d3b553813cd3e7bedb12"
 
   auto_updates true
 
--- Casks/chatall-intel.rb
+++ Casks/chatall-intel.rb
@@ -1,11 +1,11 @@
 cask "chatall-intel" do
-  version "v1.78.103"
+  version "v1.78.104"
 
-  url "https://github.com/sunner/ChatALL/releases/download/v1.78.103/ChatALL-1.78.103-mac-x64.dmg"
+  url "https://github.com/sunner/ChatALL/releases/download/v1.78.104/ChatALL-1.78.104-mac-x64.dmg"
   name "ChatALL-intel"
   desc "Concurrently chat with ChatGPT, Bing Chat, bard, Alpaca, Vincuna, Claude, ChatGLM, MOSS, iFlytek Spark, ERNIE and more, discover the best answers"
   homepage "https://github.com/sunner/ChatALL"
-  sha256 "54f166a46bc854f151934c019c489917dca571ef663d25a35ccc3c6584bda36a"
+  sha256 "8202452b337bd5146907e25ae1e1e5b4b9f73d7866298e3d2514d5fd0574c0c0"
 
   auto_updates true
 
