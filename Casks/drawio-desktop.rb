64760d4cd03dc1b11d36467405145438af5671ae
GitHub Action <action@github.com>
2024-06-08 01:18:09 +0000

    Automated update

--- Casks/drawio-desktop.rb
+++ Casks/drawio-desktop.rb
@@ -1,11 +1,11 @@
 cask "drawio-desktop" do
-  version "v24.4.13"
+  version "v24.5.1"
 
-  url "https://github.com/jgraph/drawio-desktop/releases/download/v24.4.13/draw.io-universal-24.4.13.dmg"
+  url "https://github.com/jgraph/drawio-desktop/releases/download/v24.5.1/draw.io-universal-24.5.1.dmg"
   name "drawio-desktop"
   desc "Official electron build of draw.io"
   homepage "https://github.com/jgraph/drawio-desktop"
-  sha256 "49a4d12d21c7fe4b83c44342176198d788e5ccdd5673e2e26aa20714829bd340"
+  sha256 "5135f278a691c7c5c8127b54037f0ad56dd06d583d4d9c36b578bdb9110f018f"
 
   auto_updates true
 
