4597ec838d246f26a8d0d23b9950ad82f8b1f2ef
GitHub Action <action@github.com>
2024-06-13 01:16:40 +0000

    Automated update

--- Casks/pass-simple.rb
+++ Casks/pass-simple.rb
@@ -1,6 +1,6 @@
 cask "pass-simple" do
-  version "1.0.9"
-  sha256 "9b4e260dab69ddb22d088caedf403d4210663d0a7fe261762ceb84c4db83e41a"
+  version "1.0.10"
+  sha256 "2ad0198c8f69b55c878737340bdc66d2f2c08ed51c2804783380a1de3826c578"
 
   url "https://downloads.sourceforge.net/pass-simple/pass-simple-#{version}-Darwin.dmg"
   name "Pass simple"
--- Casks/pulp.rb
+++ Casks/pulp.rb
@@ -1,11 +1,11 @@
 cask "pulp" do
-  version "v1.1.1"
+  version "v1.2.1"
 
-  url "https://github.com/vincehi/pulp/releases/download/v1.1.1/pulp_1.1.1_x64.dmg"
+  url "https://github.com/vincehi/pulp/releases/download/v1.2.1/pulp_1.2.1_x64.dmg"
   name "pulp"
   desc "Sample manager"
   homepage "https://github.com/bazalp/pulp"
-  sha256 "9b46355bc788d06be2048a095d2702d66fd7a1f56aad67af52fba1e67b138db3"
+  sha256 "05fe5d8d06aa3218c69870dbc65df2d72e39d2dc04c021a941b50b1a91b584c4"
 
   auto_updates true
 
