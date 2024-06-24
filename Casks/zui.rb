33a76513c56b092eecf618a72b169a3a026699d6
GitHub Action <action@github.com>
2024-06-24 01:18:03 +0000

    Automated update

--- Casks/zui.rb
+++ Casks/zui.rb
@@ -1,11 +1,11 @@
 cask "zui" do
-  version "v1.7.0"
+  version "v1.8.0"
 
-  url "https://github.com/brimdata/zui/releases/download/v1.7.0/Zui-1.7.0-mac.zip"
+  url "https://github.com/brimdata/zui/releases/download/v1.8.0/Zui-1.8.0-mac.zip"
   name "zui"
   desc "Zui is a powerful desktop application for exploring and working with data. The official front-end to the Zed lake."
   homepage "https://github.com/brimdata/zui"
-  sha256 "e9424aaabb4b76afe1428e0f8ee5faac9f108ce312792f0a68e49bd5684de358"
+  sha256 "6e446b5c32cca866ecade79b054bbb91d3bfcd597206d24d17449ed0a8f0c143"
 
   auto_updates true
 
