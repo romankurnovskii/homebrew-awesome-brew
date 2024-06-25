509ce47d7213ffcd1a6d0ffb679547c42afc61de
GitHub Action <action@github.com>
2024-06-25 01:16:56 +0000

    Automated update

--- Casks/pulp.rb
+++ Casks/pulp.rb
@@ -1,11 +1,11 @@
 cask "pulp" do
-  version "v1.3.0"
+  version "v1.3.1"
 
-  url "https://github.com/vincehi/pulp/releases/download/v1.3.0/pulp_1.3.0_x64.dmg"
+  url "https://github.com/vincehi/pulp/releases/download/v1.3.1/pulp_1.3.1_x64.dmg"
   name "pulp"
   desc "Sample manager"
   homepage "https://github.com/bazalp/pulp"
-  sha256 "384e7906dc3f18f9f8c4d8556a86559614e5bfa3845e28b294a621ab6772a04d"
+  sha256 "01c08fa264ada0bccbb99074547e3e5f91e4a9cc6628195ba85b82ebe9c7794f"
 
   auto_updates true
 
