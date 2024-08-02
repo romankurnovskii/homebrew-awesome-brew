e133d0a4bd4943e4dea7d2e41daf14ee7f9df1df
GitHub Action <action@github.com>
2024-08-02 01:19:41 +0000

    Automated update

--- Casks/encrypt0r-intel.rb
+++ Casks/encrypt0r-intel.rb
@@ -1,11 +1,11 @@
 cask "encrypt0r-intel" do
-  version "v3.12.48"
+  version "v3.12.49"
 
-  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.48/encrypt0r-mac.zip"
+  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.49/encrypt0r-mac.zip"
   name "encrypt0r-intel"
   desc "encrypt0r App to encrypt and decrypt your files with a passphrase"
   homepage "https://github.com/kunalnagar/encrypt0r"
-  sha256 "94501a5fb8f461d42834b4ff47e373e7f19a80094c6609bb7c8cbceb876747b7"
+  sha256 "cd0248e4e0505dff76c11fa066abe4e6180fba09a1a6a9350885bd15c8906f6c"
 
   auto_updates true
 
--- Casks/ollama.rb
+++ Casks/ollama.rb
@@ -1,11 +1,11 @@
 cask "ollama" do
-  version "v0.3.1"
+  version "v0.3.2"
 
-  url "https://github.com/ollama/ollama/releases/download/v0.3.1/Ollama-darwin.zip"
+  url "https://github.com/ollama/ollama/releases/download/v0.3.2/Ollama-darwin.zip"
   name "ollama"
   desc "ollama - get up and running with Llama 2, Mistral, and other large language models locally"
   homepage "https://github.com/ollama/ollama"
-  sha256 "0ac7e80771d42878206cfda15292abffe3408ae174aef653176e7b41f85c1e58"
+  sha256 "0568d218190b1c743f287b05c543bc0929cd47c32282fcbd5bb997752b0dec42"
 
   auto_updates true
 
