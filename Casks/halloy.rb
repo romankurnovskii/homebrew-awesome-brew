f6595a6f728ff4d2f61bc9242eb481f0e43033dd
GitHub Action <action@github.com>
2024-08-05 01:22:41 +0000

    Automated update

--- Casks/halloy.rb
+++ Casks/halloy.rb
@@ -1,11 +1,11 @@
 cask "halloy" do
-  version "2024.9"
+  version "2024.10"
 
-  url "https://github.com/squidowl/halloy/releases/download/2024.9/halloy.dmg"
+  url "https://github.com/squidowl/halloy/releases/download/2024.10/halloy.dmg"
   name "halloy"
   desc "IRC application written in Rust"
   homepage "https://github.com/squidowl/halloy"
-  sha256 "f0cbf8c2dd2caf990b9795735d05ef5d975bf334fe2e2aab34a1980cc442cbe3"
+  sha256 "af488f6cee9db2f7a018cd1ad3874301476e5b9691db0a2ab3339690ab2c0f1b"
 
   auto_updates true
 
