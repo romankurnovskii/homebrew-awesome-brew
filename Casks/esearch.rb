16f4d98e7886f910ba42918768997e58be573054
GitHub Action <action@github.com>
2024-08-13 01:23:45 +0000

    Automated update

--- Casks/esearch.rb
+++ Casks/esearch.rb
@@ -1,11 +1,11 @@
 cask "esearch" do
-  version "1.12.1"
+  version "1.12.3"
 
-  url "https://github.com/xushengfeng/eSearch/releases/download/1.12.1/eSearch-1.12.1-darwin-x64.dmg"
+  url "https://github.com/xushengfeng/eSearch/releases/download/1.12.3/eSearch-1.12.3-darwin-x64.dmg"
   name "eSearch"
   desc "Screenshot OCR search translate search for picture paste the picture on the screen screen recorder"
   homepage "https://github.com/xushengfeng/eSearch"
-  sha256 "8a021e6f750fb7c32479212a6400150df79ce10b78553b8f834f4bb1ae8e431b"
+  sha256 "bdd1ff4d4a690a703e4e18a5c67532a337762e610e693614dec6c5b6e92f71de"
 
   auto_updates true
 
