6679e13a2be11b83bb2c1dfdc053c1bffc38fbe1
GitHub Action <action@github.com>
2024-08-15 01:20:42 +0000

    Automated update

--- Casks/affine-intel.rb
+++ Casks/affine-intel.rb
@@ -1,11 +1,11 @@
 cask "affine-intel" do
-  version "v0.16.2"
+  version "v0.16.3"
 
-  url "https://github.com/toeverything/AFFiNE/releases/download/v0.16.2/affine-0.16.2-stable-macos-x64.dmg"
+  url "https://github.com/toeverything/AFFiNE/releases/download/v0.16.3/affine-0.16.3-stable-macos-x64.dmg"
   name "AFFiNE-intel"
   desc "Knowledge base that brings planning, sorting and creating all together. Notion and Miro."
   homepage "https://github.com/toeverything/AFFiNE"
-  sha256 "10de9a3fb95d362c9722c5f69a3e4abfadf2e08c2e50f4d21276d039c23e9b59"
+  sha256 "1a9cc9d38e867d767a1157137a0efa69088272334f837c397111d9e5b7a157b6"
 
   auto_updates true
 
--- Casks/diffusionbee-stable-diffusion-ui-arm.rb
+++ Casks/diffusionbee-stable-diffusion-ui-arm.rb
@@ -1,11 +1,11 @@
 cask "diffusionbee-stable-diffusion-ui-arm" do
-  version "2.5.1"
+  version "2.5.3"
 
-  url "https://github.com/divamgupta/diffusionbee-stable-diffusion-ui/releases/download/2.5.1/DiffusionBee_MPS_arm64-2.5.1.dmg"
+  url "https://github.com/divamgupta/diffusionbee-stable-diffusion-ui/releases/download/2.5.3/DiffusionBee_MPS_arm64-2.5.3.dmg"
   name "diffusionbee-stable-diffusion-ui-arm"
   desc "Diffusion is the easiest way to run Stable Diffusion locally on your M1 Mac. Comes with a one-click installer. No dependencies or technical knowledge needed."
   homepage "https://github.com/divamgupta/diffusionbee-stable-diffusion-ui"
-  sha256 "f11d9ba5563ea8c3ed3bdd20ca033e7fb56e85496e0110d89889f51d8f168dea"
+  sha256 "1c6deb9f4c745ca86631cc830951b69d5359a13bad13e87e34712617881c2977"
 
   auto_updates true
 
