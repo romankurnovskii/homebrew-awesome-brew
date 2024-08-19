e8c61f8707ff770958a10ab24ddd0c795868ea41
GitHub Action <action@github.com>
2024-08-19 01:22:25 +0000

    Automated update

--- Casks/uyou-todo-electron-arm.rb
+++ Casks/uyou-todo-electron-arm.rb
@@ -1,11 +1,11 @@
 cask "uyou-todo-electron-arm" do
-  version "3.0.2"
+  version "3.0.3"
 
-  url "https://github.com/tonylu110/uyou-todo-electron/releases/download/3.0.2/uyou.ToDo-3.0.2-arm64.dmg"
+  url "https://github.com/tonylu110/uyou-todo-electron/releases/download/3.0.3/uyou.ToDo-3.0.3-arm64.dmg"
   name "uyou-todo-electron-arm"
   desc "Robotics visualization and debugging"
   homepage "https://github.com/tonylu110/uyou-todo-electron"
-  sha256 "b149ee46bc737f784f9e905a5463fe3001e6920758cdc817f3a45e2916c5d00f"
+  sha256 "aab870992a5f2ffbf87d22bd1e574a2e046c11a8c3c027b90ff333f35b2bf18f"
 
   auto_updates true
 
