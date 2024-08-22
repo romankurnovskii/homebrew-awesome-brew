fdff43852600548eff18bfc93db6e3aef4c4232e
GitHub Action <action@github.com>
2024-08-22 01:23:55 +0000

    Automated update

--- Casks/antares.rb
+++ Casks/antares.rb
@@ -1,11 +1,11 @@
 cask "antares" do
-  version "v0.7.27"
+  version "v0.7.28"
 
-  url "https://github.com/antares-sql/antares/releases/download/v0.7.27/Antares-0.7.27-mac_x64.dmg"
+  url "https://github.com/antares-sql/antares/releases/download/v0.7.28/Antares-0.7.28-mac_x64.dmg"
   name "antares"
   desc "A modern, fast and productivity driven SQL client with a focus in UX."
   homepage "https://github.com/antares-sql/antares"
-  sha256 "6a437288af4904aee583c15fa167c0dabb77e04cc7aadd3af2159c7597d8e419"
+  sha256 "9e9654aa18203b2fc51892b766811e7ed12c4033ded1289e733d0ba62a0888d1"
 
   auto_updates true
 
