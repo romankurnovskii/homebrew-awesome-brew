4bdfbd9188d837af49934e283146e3d231c3d300
GitHub Action <action@github.com>
2024-09-15 01:41:22 +0000

    Automated update

--- Casks/alphabiz.rb
+++ Casks/alphabiz.rb
@@ -1,11 +1,11 @@
 cask "alphabiz" do
-  version "0.3.0"
+  version "0.3.3"
 
-  url "https://github.com/tanshuai/alphabiz/releases/download/0.3.0/alphabiz-x64-0.3.0.dmg"
+  url "https://github.com/tanshuai/alphabiz/releases/download/0.3.3/alphabiz-x64-0.3.3.dmg"
   name "alphabiz"
   desc "Web3 ecosystem - enable developers to build fully decentralized media platform and blockchain-based marketplace"
   homepage "https://github.com/tanshuai/alphabiz"
-  sha256 "5df904d165000de7cb31ef12d9dfce7b5afccc2bf9451845c57085fd0b0fd097"
+  sha256 "bf603f8c71362486b471768f5a9665245520c336ce7905ef76a887844718dcb3"
 
   auto_updates true
 
