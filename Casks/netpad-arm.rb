dfff9ae2702344be9c779d9fd6143230a41b11da
GitHub Action <action@github.com>
2024-10-02 01:36:58 +0000

    Automated update

--- Casks/netpad-arm.rb
+++ Casks/netpad-arm.rb
@@ -1,11 +1,11 @@
 cask "netpad-arm" do
-  version "v0.7.2"
+  version "v0.8.0"
 
-  url "https://github.com/tareqimbasher/NetPad/releases/download/v0.7.2/netpad-0.7.2-mac-arm64.zip"
+  url "https://github.com/tareqimbasher/NetPad/releases/download/v0.8.0/netpad-0.8.0-mac-arm64.zip"
   name "NetPad-arm"
   desc "Robotics visualization and debugging"
   homepage "https://github.com/tareqimbasher/NetPad"
-  sha256 "db2514c12f0635c9d5793359199184841ad7ac9fd929ad2a3d58b33de88a5ea5"
+  sha256 "17675a85d14cd42f2b518639f587c567b63ac6f358962437cb040d9cc6028966"
 
   auto_updates true
 
