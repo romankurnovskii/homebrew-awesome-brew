9f90f15d6ee181e1cace9a4927fd835200a4f116
GitHub Action <action@github.com>
2024-11-10 01:41:44 +0000

    Automated update

--- Casks/final2x-intel.rb
+++ Casks/final2x-intel.rb
@@ -1,11 +1,11 @@
 cask "final2x-intel" do
-  version "2024-10-28"
+  version "2024-11-09"
 
-  url "https://github.com/Tohrusky/Final2x/releases/download/2024-10-28/Final2x-macos-x64-dmg.dmg"
+  url "https://github.com/Tohrusky/Final2x/releases/download/2024-11-09/Final2x-macos-x64-dmg.dmg"
   name "Final2x-intel"
   desc "2^x Image Super-Resolution"
   homepage "https://github.com/Tohrusky/Final2x"
-  sha256 "7a15bb2f9c6d843273da2a2484c712bd2bf28ca6bdee3ce0c4bd6b5ed101fb4c"
+  sha256 "e2cd741bd8efe01e99aa04db5cdf67a621ff1ee8f82f4d088395a1299ca860bd"
 
   auto_updates true
 
