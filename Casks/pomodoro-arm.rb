4b8e950a2f3e4faa7cdd092adb4831b14176c7e8
GitHub Action <action@github.com>
2024-11-17 01:46:25 +0000

    Automated update

--- Casks/pomodoro-arm.rb
+++ Casks/pomodoro-arm.rb
@@ -1,11 +1,11 @@
 cask "pomodoro-arm" do
-  version "v3.5.0"
+  version "v3.6.0"
 
-  url "https://github.com/G07cha/pomodoro/releases/download/v3.5.0/Pomodoro_3.5.0_aarch64.dmg"
+  url "https://github.com/G07cha/pomodoro/releases/download/v3.6.0/Pomodoro_3.6.0_aarch64.dmg"
   name "pomodoro-arm"
   desc "Pomodoro time managment tool build with Tauri"
   homepage "https://github.com/G07cha/pomodoro"
-  sha256 "66ab5709b92b6d9735df32931f4a64d4ec0c40c5f20701a1cacd7d6241eb92c1"
+  sha256 "748b2f726c6e9620339c66469778815879ca88463cc391a0e3c460e759b0d55c"
 
   auto_updates true
 
