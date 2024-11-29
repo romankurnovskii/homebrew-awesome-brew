65415425772fc8a337547ef9966e7c00e1d91216
GitHub Action <action@github.com>
2024-11-29 01:47:07 +0000

    Automated update

--- Casks/affine-arm.rb
+++ Casks/affine-arm.rb
@@ -1,11 +1,11 @@
 cask "affine-arm" do
-  version "v0.18.1"
+  version "v0.18.2"
 
-  url "https://github.com/toeverything/AFFiNE/releases/download/v0.18.1/affine-0.18.1-stable-macos-arm64.dmg"
+  url "https://github.com/toeverything/AFFiNE/releases/download/v0.18.2/affine-0.18.2-stable-macos-arm64.dmg"
   name "AFFiNE-arm"
   desc "Knowledge base that brings planning, sorting and creating all together. Notion and Miro."
   homepage "https://github.com/toeverything/AFFiNE"
-  sha256 "2bacfe0824067ea9c89b40100e81ba7e3aa118df0e1be5fd34c7e6daf64807ef"
+  sha256 "52d497dd4824a5cc10049135cba3aca8747a5e0c5853157d10380bc053b049f6"
 
   auto_updates true
 
--- Casks/ollama.rb
+++ Casks/ollama.rb
@@ -1,11 +1,11 @@
 cask "ollama" do
-  version "v0.4.5"
+  version "v0.4.6"
 
-  url "https://github.com/ollama/ollama/releases/download/v0.4.5/Ollama-darwin.zip"
+  url "https://github.com/ollama/ollama/releases/download/v0.4.6/Ollama-darwin.zip"
   name "ollama"
   desc "ollama - get up and running with Llama 2, Mistral, and other large language models locally"
   homepage "https://github.com/ollama/ollama"
-  sha256 "85e3075a4f1f5e3c532ab015badef379078d19c946f7b195b8ae04f984f7493c"
+  sha256 "1db1e40d5a5d05b5898ef6c2f79fc904e58d4dfa6077987040336cbdc0dd4ec9"
 
   auto_updates true
 
