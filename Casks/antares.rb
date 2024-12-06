5e7eb77b35d10d6fda9b5dcd7f9c5484f56d44a7
GitHub Action <action@github.com>
2024-12-06 01:45:14 +0000

    Automated update

--- Casks/antares.rb
+++ Casks/antares.rb
@@ -1,11 +1,11 @@
 cask "antares" do
-  version "v0.7.29"
+  version "v0.7.30"
 
-  url "https://github.com/antares-sql/antares/releases/download/v0.7.29/Antares-0.7.29-mac_x64.dmg"
+  url "https://github.com/antares-sql/antares/releases/download/v0.7.30/Antares-0.7.30-mac_x64.dmg"
   name "antares"
   desc "A modern, fast and productivity driven SQL client with a focus in UX."
   homepage "https://github.com/antares-sql/antares"
-  sha256 "8567b137b0328e86bd00c0e43334c63f4902923923364725c99e8d222df319bd"
+  sha256 "72b0dc02143f793cdb51ef33cc9e069e16ba0a858c9e3baa9944f1b3624af25a"
 
   auto_updates true
 
--- Casks/brewmate.rb
+++ Casks/brewmate.rb
@@ -1,11 +1,11 @@
 cask "brewmate" do
-  version "0.4.2"
+  version "0.4.3"
 
-  url "https://github.com/romankurnovskii/BrewMate/releases/download/0.4.2/BrewMate-0.4.2-universal-mac.zip"
+  url "https://github.com/romankurnovskii/BrewMate/releases/download/0.4.3/BrewMate-0.4.3-universal-mac.zip"
   name "BrewMate"
   desc "Homebrew GUI apps manager"
   homepage "https://github.com/romankurnovskii/BrewMate"
-  sha256 "17e7453cc51cbfc7272308a86ad5004438790fdd06b85d78baf58591728b76b0"
+  sha256 "8bae065be6b4a3f113a651ffffd632b7a7ea40f4b608b1fc90c058905de3d85b"
 
   auto_updates true
 
