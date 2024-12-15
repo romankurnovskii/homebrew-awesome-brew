e8fb4fb1ed6488331e2ea68f3877ab4e19caaf19
GitHub Action <action@github.com>
2024-12-15 01:52:25 +0000

    Automated update

--- Casks/final2x-arm.rb
+++ Casks/final2x-arm.rb
@@ -1,11 +1,11 @@
 cask "final2x-arm" do
-  version "2024-11-09"
+  version "2024-12-14"
 
-  url "https://github.com/Tohrusky/Final2x/releases/download/2024-11-09/Final2x-macos-arm64-dmg.dmg"
+  url "https://github.com/Tohrusky/Final2x/releases/download/2024-12-14/Final2x-macos-arm64-dmg.dmg"
   name "Final2x-arm"
   desc "2^x Image Super-Resolution"
   homepage "https://github.com/Tohrusky/Final2x"
-  sha256 "6357da67b94504128412cd8de2047aa1a3f21a02ba99d822f46a811365cced18"
+  sha256 "a28d60ac4d7c1bed3207b44fbd8ff4ac2403256bfc2c2599ee0487c7bf3f7c51"
 
   auto_updates true
 
--- Casks/final2x-intel.rb
+++ Casks/final2x-intel.rb
@@ -1,11 +1,11 @@
 cask "final2x-intel" do
-  version "2024-11-09"
+  version "2024-12-14"
 
-  url "https://github.com/Tohrusky/Final2x/releases/download/2024-11-09/Final2x-macos-x64-dmg.dmg"
+  url "https://github.com/Tohrusky/Final2x/releases/download/2024-12-14/Final2x-macos-x64-dmg.dmg"
   name "Final2x-intel"
   desc "2^x Image Super-Resolution"
   homepage "https://github.com/Tohrusky/Final2x"
-  sha256 "e2cd741bd8efe01e99aa04db5cdf67a621ff1ee8f82f4d088395a1299ca860bd"
+  sha256 "429aeebcc458e477ae4236001f1e274c2bcb14722d4baf601db307972239e16b"
 
   auto_updates true
 
