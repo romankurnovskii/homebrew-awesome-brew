03ea307277087d6a4ab075f6841a1bce9fbb2d59
GitHub Action <action@github.com>
2025-01-04 01:32:00 +0000

    Automated update

--- Casks/drawio-desktop.rb
+++ Casks/drawio-desktop.rb
@@ -1,11 +1,11 @@
 cask "drawio-desktop" do
-  version "v25.0.2"
+  version "v26.0.3"
 
-  url "https://github.com/jgraph/drawio-desktop/releases/download/v25.0.2/draw.io-universal-25.0.2.dmg"
+  url "https://github.com/jgraph/drawio-desktop/releases/download/v26.0.3/draw.io-universal-26.0.3.dmg"
   name "drawio-desktop"
   desc "Official electron build of draw.io"
   homepage "https://github.com/jgraph/drawio-desktop"
-  sha256 "ab9c5bcea90ca3080fbc0f4195b768475a4acb17a2bf541b892dd259aac81559"
+  sha256 "5d1d3c53960c94f121340923ec8b2c53f36bfc68d09822b06b98c73eb26c0e77"
 
   auto_updates true
 
