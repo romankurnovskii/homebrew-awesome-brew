0a5780320d405aeaa29e3563a1b99a7b89d8261d
GitHub Action <action@github.com>
2025-02-13 01:38:25 +0000

    Automated update

--- Casks/antares.rb
+++ Casks/antares.rb
@@ -1,11 +1,11 @@
 cask "antares" do
-  version "v0.7.30"
+  version "v0.7.31"
 
-  url "https://github.com/antares-sql/antares/releases/download/v0.7.30/Antares-0.7.30-mac_x64.dmg"
+  url "https://github.com/antares-sql/antares/releases/download/v0.7.31/Antares-0.7.31-mac_x64.dmg"
   name "antares"
   desc "A modern, fast and productivity driven SQL client with a focus in UX."
   homepage "https://github.com/antares-sql/antares"
-  sha256 "72b0dc02143f793cdb51ef33cc9e069e16ba0a858c9e3baa9944f1b3624af25a"
+  sha256 "6afb210ea5fe87cc65775fd3d98bb570e4a1dfe787fff3c66fac1037bb7dac7b"
 
   auto_updates true
 
--- Casks/lapki-client.rb
+++ Casks/lapki-client.rb
@@ -1,11 +1,11 @@
 cask "lapki-client" do
-  version "v0.3.2"
+  version "v0.4.0"
 
-  url "https://github.com/kruzhok-team/lapki-client/releases/download/v0.3.2/lapki-ide-v0.3.2.dmg"
+  url "https://github.com/kruzhok-team/lapki-client/releases/download/v0.4.0/lapki-ide-v0.4.0.dmg"
   name "lapki-client"
   desc "Lapki IDE - Графическая IDE для киберфизических систем"
   homepage "https://github.com/kruzhok-team/lapki-client"
-  sha256 "b63ea3d5a950413469c7d84ccbdd29e40ea388af0ff7ff84a7ad10975b514a2d"
+  sha256 "aefab3a23b318de1927f01104097a9787c86c2f15f95578f7a9ecca44b59ff18"
 
   auto_updates true
 
