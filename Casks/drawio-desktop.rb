0a00f0f55e0c9d96b8b1db7aa2f9267125d2e142
GitHub Action <action@github.com>
2025-02-25 01:40:49 +0000

    Automated update

--- Casks/drawio-desktop.rb
+++ Casks/drawio-desktop.rb
@@ -1,11 +1,11 @@
 cask "drawio-desktop" do
-  version "v26.0.15"
+  version "v26.0.16"
 
-  url "https://github.com/jgraph/drawio-desktop/releases/download/v26.0.15/draw.io-universal-26.0.15.dmg"
+  url "https://github.com/jgraph/drawio-desktop/releases/download/v26.0.16/draw.io-universal-26.0.16.dmg"
   name "drawio-desktop"
   desc "Official electron build of draw.io"
   homepage "https://github.com/jgraph/drawio-desktop"
-  sha256 "8d65798c47d7c9ccd380b9357a0050d37900e72ab9ba44b9ecd72442c8d1adef"
+  sha256 "1b022a16af0e02d12b9ce7b62a8aeafdadfe78fdad1c45afe686e9422ad24bc0"
 
   auto_updates true
 
--- Casks/ollama.rb
+++ Casks/ollama.rb
@@ -1,11 +1,11 @@
 cask "ollama" do
-  version "v0.5.11"
+  version "v0.5.12"
 
-  url "https://github.com/ollama/ollama/releases/download/v0.5.11/Ollama-darwin.zip"
+  url "https://github.com/ollama/ollama/releases/download/v0.5.12/Ollama-darwin.zip"
   name "ollama"
   desc "ollama - get up and running with Llama 2, Mistral, and other large language models locally"
   homepage "https://github.com/ollama/ollama"
-  sha256 "32a0863638e2a842a35779392667c4ae7850f0d4f347d4fb52feed1526054791"
+  sha256 "ad7ee1b95a2e44c55a1313f7bdb02e192df9fa71a5c31f6ea55c5694c3a2d994"
 
   auto_updates true
 
