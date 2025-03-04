6fa576bde40269cc9caaea10d8de5f3eafa8f381
GitHub Action <action@github.com>
2025-03-04 01:43:19 +0000

    Automated update

--- Casks/affine-arm.rb
+++ Casks/affine-arm.rb
@@ -1,11 +1,11 @@
 cask "affine-arm" do
-  version "v0.20.3"
+  version "v0.20.4"
 
-  url "https://github.com/toeverything/AFFiNE/releases/download/v0.20.3/affine-0.20.3-stable-macos-arm64.dmg"
+  url "https://github.com/toeverything/AFFiNE/releases/download/v0.20.4/affine-0.20.4-stable-macos-arm64.dmg"
   name "AFFiNE-arm"
   desc "Knowledge base that brings planning, sorting and creating all together. Notion and Miro."
   homepage "https://github.com/toeverything/AFFiNE"
-  sha256 "2ea91e7b71a2f7a3b179f6e1b2a4344e6cc49fbfdaa68a083ee09d63e5e6d836"
+  sha256 "4d45dc7bdecb94f5f1c60b2178302734801d8f276279a121792f730f5186f27b"
 
   auto_updates true
 
--- Casks/books.rb
+++ Casks/books.rb
@@ -1,11 +1,11 @@
 cask "books" do
-  version "v0.27.0"
+  version "v0.28.0"
 
-  url "https://github.com/frappe/books/releases/download/v0.27.0/Frappe-Books-0.27.0-mac.zip"
+  url "https://github.com/frappe/books/releases/download/v0.28.0/Frappe-Books-0.28.0-mac.zip"
   name "books"
   desc "Free Accounting Software"
   homepage "https://github.com/frappe/books"
-  sha256 "e1c26cac1f4d8fa1779b5aa07c3568fdadc2f2860c99216f16b7271f52d2c248"
+  sha256 "cdf258f105948f11d9e8fea889bb8e4b372cf4e6a9b7f5910654dd93908339d3"
 
   auto_updates true
 
