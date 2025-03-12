f0dbfb5f4d2145c1aba50fc8c37396cf131fad37
GitHub Action <action@github.com>
2025-03-12 18:26:15 +0000

    Automated update

--- Casks/onlook-intel.rb
+++ Casks/onlook-intel.rb
@@ -1,11 +1,11 @@
 cask "onlook-intel" do
-  version "v0.2.12"
+  version "v0.2.13"
 
-  url "https://github.com/onlook-dev/onlook/releases/download/v0.2.12/Onlook-x64.dmg"
+  url "https://github.com/onlook-dev/onlook/releases/download/v0.2.13/Onlook-x64.dmg"
   name "onlook-intel"
   desc "Cursor for Designers. Design directly in your live React app and publish your changes to code onlook.com"
   homepage "https://github.com/onlook-dev/onlook"
-  sha256 "914f95480b23e3263233ed282af5786a01cba0f32b1bf855aa5134b878adf314"
+  sha256 "6f046e1656f2fc91edbd6357e993ef10715ce06b691d23d3527b550b0ffd37ba"
 
   auto_updates true
 
