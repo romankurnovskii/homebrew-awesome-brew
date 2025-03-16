eef242a6198115afd26db12b01001e5f36fd6a39
GitHub Action <action@github.com>
2025-03-16 01:48:57 +0000

    Automated update

--- Casks/halloy.rb
+++ Casks/halloy.rb
@@ -1,11 +1,11 @@
 cask "halloy" do
-  version "2025.2"
+  version "2025.3"
 
-  url "https://github.com/squidowl/halloy/releases/download/2025.2/halloy.dmg"
+  url "https://github.com/squidowl/halloy/releases/download/2025.3/halloy.dmg"
   name "halloy"
   desc "IRC application written in Rust"
   homepage "https://github.com/squidowl/halloy"
-  sha256 "84786cf29f79ee96f54bc09c3bb6321ac6f3d171841d1b32cdd99a948ff524b7"
+  sha256 "af6d3b3fd6005c72eb0e8b6bd27c343bbfc392ca86fd08840883f01493826a55"
 
   auto_updates true
 
--- Casks/onlook-arm.rb
+++ Casks/onlook-arm.rb
@@ -1,11 +1,11 @@
 cask "onlook-arm" do
-  version "v0.2.14"
+  version "v0.2.15"
 
-  url "https://github.com/onlook-dev/onlook/releases/download/v0.2.14/Onlook-arm64.dmg"
+  url "https://github.com/onlook-dev/onlook/releases/download/v0.2.15/Onlook-arm64.dmg"
   name "onlook-arm"
   desc "Cursor for Designers. Design directly in your live React app and publish your changes to code onlook.com"
   homepage "https://github.com/onlook-dev/onlook"
-  sha256 "5d9577a8c96f144c658fd8cc640a8db4d443f1f3394d796c619103cda5c77e0c"
+  sha256 "6a6facf76ce22da0ca26c6789235fa59c889b60b8e860d04a2c578e24c57afbd"
 
   auto_updates true
 
