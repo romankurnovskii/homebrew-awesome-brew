ee677d881336a2f2e6596eab7a4e7c06a2d807d6
GitHub Action <action@github.com>
2025-03-17 01:49:13 +0000

    Automated update

--- Casks/chatall-arm.rb
+++ Casks/chatall-arm.rb
@@ -1,11 +1,11 @@
 cask "chatall-arm" do
-  version "v1.83.108"
+  version "v1.84.109"
 
-  url "https://github.com/ai-shifu/ChatALL/releases/download/v1.83.108/ChatALL-1.83.108-mac-arm64.dmg"
+  url "https://github.com/ai-shifu/ChatALL/releases/download/v1.84.109/ChatALL-1.84.109-mac-arm64.dmg"
   name "ChatALL-arm"
   desc "Concurrently chat with ChatGPT, Bing Chat, bard, Alpaca, Vincuna, Claude, ChatGLM, MOSS, iFlytek Spark, ERNIE and more, discover the best answers"
   homepage "https://github.com/sunner/ChatALL"
-  sha256 "4382cd9b040931437af5bc2fc626e3b1d421a8b6f80600e43363b757ede16f6d"
+  sha256 "931f0ef6a229857ae6af0a243400244981d0fd07088cba2174099424f265a927"
 
   auto_updates true
 
--- Casks/drawio-desktop.rb
+++ Casks/drawio-desktop.rb
@@ -1,11 +1,11 @@
 cask "drawio-desktop" do
-  version "v26.0.16"
+  version "v26.1.1"
 
-  url "https://github.com/jgraph/drawio-desktop/releases/download/v26.0.16/draw.io-universal-26.0.16.dmg"
+  url "https://github.com/jgraph/drawio-desktop/releases/download/v26.1.1/draw.io-universal-26.1.1.dmg"
   name "drawio-desktop"
   desc "Official electron build of draw.io"
   homepage "https://github.com/jgraph/drawio-desktop"
-  sha256 "1b022a16af0e02d12b9ce7b62a8aeafdadfe78fdad1c45afe686e9422ad24bc0"
+  sha256 "ce03dfa2514e0d70eccc2d53881447a175d2cefd9b66117d94bd934ab4567c3b"
 
   auto_updates true
 
