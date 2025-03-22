90bb8865c7e5ee9150dc995e76a8aade516df818
GitHub Action <action@github.com>
2025-03-22 01:46:36 +0000

    Automated update

--- Casks/lapki-client.rb
+++ Casks/lapki-client.rb
@@ -1,11 +1,11 @@
 cask "lapki-client" do
-  version "v0.4.1"
+  version "v0.4.3"
 
-  url "https://github.com/kruzhok-team/lapki-client/releases/download/v0.4.1/lapki-ide-v0.4.1.dmg"
+  url "https://github.com/kruzhok-team/lapki-client/releases/download/v0.4.3/lapki-ide-v0.4.3.dmg"
   name "lapki-client"
   desc "Lapki IDE - Графическая IDE для киберфизических систем"
   homepage "https://github.com/kruzhok-team/lapki-client"
-  sha256 "3518de8067fdb65025be76e8e7f8eb76153d126f0272e414f040aedbaf54d980"
+  sha256 "73df9e09d9284bc41f98fb090b7e63b42e7907938199a086eace288180e87212"
 
   auto_updates true
 
