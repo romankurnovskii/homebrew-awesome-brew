f9c66e4c3e4ca97cb9a82156f55b36250e98cf1e
GitHub Action <action@github.com>
2025-03-26 01:45:36 +0000

    Automated update

--- Casks/onlook-arm.rb
+++ Casks/onlook-arm.rb
@@ -1,11 +1,11 @@
 cask "onlook-arm" do
-  version "v0.2.20"
+  version "v0.2.21"
 
-  url "https://github.com/onlook-dev/onlook/releases/download/v0.2.20/Onlook-arm64.dmg"
+  url "https://github.com/onlook-dev/onlook/releases/download/v0.2.21/Onlook-arm64.dmg"
   name "onlook-arm"
   desc "Cursor for Designers. Design directly in your live React app and publish your changes to code onlook.com"
   homepage "https://github.com/onlook-dev/onlook"
-  sha256 "7f557a0f9c50832a251fd3ee6ffbd230af476d5b5c219d5b3c86162a755be887"
+  sha256 "d25090eca9e609e919effcb6eda6367432bcfd16e9562c091396b785d9bfbd0d"
 
   auto_updates true
 
--- Casks/onlook-intel.rb
+++ Casks/onlook-intel.rb
@@ -1,11 +1,11 @@
 cask "onlook-intel" do
-  version "v0.2.20"
+  version "v0.2.21"
 
-  url "https://github.com/onlook-dev/onlook/releases/download/v0.2.20/Onlook-x64.dmg"
+  url "https://github.com/onlook-dev/onlook/releases/download/v0.2.21/Onlook-x64.dmg"
   name "onlook-intel"
   desc "Cursor for Designers. Design directly in your live React app and publish your changes to code onlook.com"
   homepage "https://github.com/onlook-dev/onlook"
-  sha256 "bad8216d41b0cb5fbcfac02324696f1add0dcb3a7c32df008d81342603dbb2d1"
+  sha256 "e8b1b3624757d9f0279627e3a1a87752294b8863dd33c218d02c3c841b3077c2"
 
   auto_updates true
 
