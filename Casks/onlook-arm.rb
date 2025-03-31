863920be9f55adaa792eb6ba0b09f0f74403bf39
GitHub Action <action@github.com>
2025-03-31 19:22:47 +0000

    Automated update

--- Casks/onlook-arm.rb
+++ Casks/onlook-arm.rb
@@ -1,11 +1,11 @@
 cask "onlook-arm" do
-  version "v0.2.22"
+  version "v0.2.23"
 
-  url "https://github.com/onlook-dev/onlook/releases/download/v0.2.22/Onlook-arm64.dmg"
+  url "https://github.com/onlook-dev/onlook/releases/download/v0.2.23/Onlook-arm64.dmg"
   name "onlook-arm"
   desc "Cursor for Designers. Design directly in your live React app and publish your changes to code onlook.com"
   homepage "https://github.com/onlook-dev/onlook"
-  sha256 "aee85126950eb51dc64d2a2a9b2147c26baf0c4b3583ff80f2ee7fddc8ebb1e7"
+  sha256 "fe50f1539b5912b908d5fecf29ee0fb1eb7e9730fdf2b6527af036aa36016e5e"
 
   auto_updates true
 
