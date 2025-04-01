a28dc86cc1f6056fb9ea42e3caf73d6a3c9001df
GitHub Action <action@github.com>
2025-04-01 01:56:55 +0000

    Automated update

--- Casks/ollama.rb
+++ Casks/ollama.rb
@@ -1,11 +1,11 @@
 cask "ollama" do
-  version "v0.6.3"
+  version "v0.6.2"
 
-  url "https://github.com/ollama/ollama/releases/download/v0.6.3/Ollama-darwin.zip"
+  url "https://github.com/ollama/ollama/releases/download/v0.6.2/Ollama-darwin.zip"
   name "ollama"
   desc "ollama - get up and running with Llama 2, Mistral, and other large language models locally"
   homepage "https://github.com/ollama/ollama"
-  sha256 "c83bd766ccdb9825427d3cf3176f8bfa836f5cea8c9221b410a1590e7dfbcaac"
+  sha256 "5219a0e2567868fcb2842a69f76dbc4b57a042286113b7363c00bd44a5c6121c"
 
   auto_updates true
 
--- Casks/onlook-arm.rb
+++ Casks/onlook-arm.rb
@@ -1,11 +1,11 @@
 cask "onlook-arm" do
-  version "v0.2.23"
+  version "v0.2.24"
 
-  url "https://github.com/onlook-dev/onlook/releases/download/v0.2.23/Onlook-arm64.dmg"
+  url "https://github.com/onlook-dev/onlook/releases/download/v0.2.24/Onlook-arm64.dmg"
   name "onlook-arm"
   desc "Cursor for Designers. Design directly in your live React app and publish your changes to code onlook.com"
   homepage "https://github.com/onlook-dev/onlook"
-  sha256 "fe50f1539b5912b908d5fecf29ee0fb1eb7e9730fdf2b6527af036aa36016e5e"
+  sha256 "45c54183c554bff64c228dc21be8856378bd2ca7150496b89562d83dcd76d080"
 
   auto_updates true
 
