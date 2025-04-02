dfb919bbc1bfba74324d24c849a0b38596cc1499
GitHub Action <action@github.com>
2025-04-02 01:50:46 +0000

    Automated update

--- Casks/drawio-desktop.rb
+++ Casks/drawio-desktop.rb
@@ -1,11 +1,11 @@
 cask "drawio-desktop" do
-  version "v26.1.1"
+  version "v26.2.2"
 
-  url "https://github.com/jgraph/drawio-desktop/releases/download/v26.1.1/draw.io-universal-26.1.1.dmg"
+  url "https://github.com/jgraph/drawio-desktop/releases/download/v26.2.2/draw.io-universal-26.2.2.dmg"
   name "drawio-desktop"
   desc "Official electron build of draw.io"
   homepage "https://github.com/jgraph/drawio-desktop"
-  sha256 "ce03dfa2514e0d70eccc2d53881447a175d2cefd9b66117d94bd934ab4567c3b"
+  sha256 "bde0604fffa8a16ab27c0e691d8af91047c40e0e6298230b0e81b226515ec0c2"
 
   auto_updates true
 
--- Casks/encrypt0r-arm.rb
+++ Casks/encrypt0r-arm.rb
@@ -1,11 +1,11 @@
 cask "encrypt0r-arm" do
-  version "v3.12.55"
+  version "v3.12.56"
 
-  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.55/encrypt0r-mac-m1.zip"
+  url "https://github.com/kunalnagar/encrypt0r/releases/download/v3.12.56/encrypt0r-mac-m1.zip"
   name "encrypt0r-arm"
   desc "encrypt0r App to encrypt and decrypt your files with a passphrase"
   homepage "https://github.com/kunalnagar/encrypt0r"
-  sha256 "1390c86641e27b14b009684e0b67e2911d0c2e4215dd85561139da0d17ddb413"
+  sha256 "b8a32b90174e9ade3290b7b271f532e42d385cd2e30de92efb2cf78d614b7b8e"
 
   auto_updates true
 
