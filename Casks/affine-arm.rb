a85550e99018f19d1fa6dc42ebfb44b810ff084a
GitHub Action <action@github.com>
2025-04-10 01:50:40 +0000

    Automated update

--- Casks/affine-arm.rb
+++ Casks/affine-arm.rb
@@ -1,11 +1,11 @@
 cask "affine-arm" do
-  version "v0.21.0"
+  version "v0.21.1"
 
-  url "https://github.com/toeverything/AFFiNE/releases/download/v0.21.0/affine-0.21.0-stable-macos-arm64.dmg"
+  url "https://github.com/toeverything/AFFiNE/releases/download/v0.21.1/affine-0.21.1-stable-macos-arm64.dmg"
   name "AFFiNE-arm"
   desc "Knowledge base that brings planning, sorting and creating all together. Notion and Miro."
   homepage "https://github.com/toeverything/AFFiNE"
-  sha256 "d4b64b59d9d0a8c70b8a890177eb927b402927c6d3ede95f3f30774b8f8a58e1"
+  sha256 "70850453cdff520624c48d86f9efbc59feb5bbb2f161b58ed1856fe5716cc299"
 
   auto_updates true
 
--- Casks/pass-simple.rb
+++ Casks/pass-simple.rb
@@ -1,7 +1,7 @@
 cask "pass-simple" do
   arch arm: "arm64", intel: "x86_64"
-  version "1.0.20"
-  sha256  arm: "75ae08347bd628bca55f6f0ff9546c194b0c2f72a7fc7546e10994b0150af2a5"
+  version "1.0.21"
+  sha256  arm: "572ca7339e9b240ea2432bb8bdcdb20e3ac988bdf0b0da327a5c21a96050668f"
           
 
   url "https://downloads.sourceforge.net/pass-simple/pass-simple-#{version}-#{arch}.dmg"
