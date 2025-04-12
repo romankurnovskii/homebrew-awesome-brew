92e57f61851c991bd875f14c92ade07328cee09b
GitHub Action <action@github.com>
2025-04-12 01:48:03 +0000

    Automated update

--- Casks/affine-arm.rb
+++ Casks/affine-arm.rb
@@ -1,11 +1,11 @@
 cask "affine-arm" do
-  version "v0.21.1"
+  version "v0.21.2"
 
-  url "https://github.com/toeverything/AFFiNE/releases/download/v0.21.1/affine-0.21.1-stable-macos-arm64.dmg"
+  url "https://github.com/toeverything/AFFiNE/releases/download/v0.21.2/affine-0.21.2-stable-macos-arm64.dmg"
   name "AFFiNE-arm"
   desc "Knowledge base that brings planning, sorting and creating all together. Notion and Miro."
   homepage "https://github.com/toeverything/AFFiNE"
-  sha256 "70850453cdff520624c48d86f9efbc59feb5bbb2f161b58ed1856fe5716cc299"
+  sha256 "080b216dcbf14197ad695e9966616aa84fb017e6da8b6bd088225d4551df8871"
 
   auto_updates true
 
