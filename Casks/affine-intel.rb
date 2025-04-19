677d5f61e705f5aa96e6d5ad74461a3d5d70eba1
GitHub Action <action@github.com>
2025-04-19 01:47:19 +0000

    Automated update

--- Casks/affine-intel.rb
+++ Casks/affine-intel.rb
@@ -1,11 +1,11 @@
 cask "affine-intel" do
-  version "v0.21.2"
+  version "v0.21.3"
 
-  url "https://github.com/toeverything/AFFiNE/releases/download/v0.21.2/affine-0.21.2-stable-macos-x64.dmg"
+  url "https://github.com/toeverything/AFFiNE/releases/download/v0.21.3/affine-0.21.3-stable-macos-x64.dmg"
   name "AFFiNE-intel"
   desc "Knowledge base that brings planning, sorting and creating all together. Notion and Miro."
   homepage "https://github.com/toeverything/AFFiNE"
-  sha256 "548071875e621c72acda8ccb2b832d263db43bc398f17a8ddadee0549e96f45c"
+  sha256 "20b756cb57c08467e9448b4e3b2fd193451c554a29c6afd03bbfe8158177e3e5"
 
   auto_updates true
 
