16f115a77c0bdeb5ff19b866a63ece67518222e1
GitHub Action <action@github.com>
2025-04-24 01:49:30 +0000

    Automated update

--- Casks/books.rb
+++ Casks/books.rb
@@ -1,11 +1,11 @@
 cask "books" do
-  version "v0.28.0"
+  version "v0.29.0"
 
-  url "https://github.com/frappe/books/releases/download/v0.28.0/Frappe-Books-0.28.0-mac.zip"
+  url "https://github.com/frappe/books/releases/download/v0.29.0/Frappe-Books-0.29.0-mac.zip"
   name "books"
   desc "Free Accounting Software"
   homepage "https://github.com/frappe/books"
-  sha256 "cdf258f105948f11d9e8fea889bb8e4b372cf4e6a9b7f5910654dd93908339d3"
+  sha256 "2a900e14a14e3687911b821f2c3cb9c8f536692f182f043dd0ab018c83bd92a2"
 
   auto_updates true
 
