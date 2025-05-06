0cfad408ff9b3eb5048f6859aeb5318ace13c04a
GitHub Action <action@github.com>
2025-05-06 01:54:49 +0000

    Automated update

--- Casks/ollama.rb
+++ Casks/ollama.rb
@@ -1,11 +1,11 @@
 cask "ollama" do
-  version "v0.6.7"
+  version "v0.6.8"
 
-  url "https://github.com/ollama/ollama/releases/download/v0.6.7/Ollama-darwin.zip"
+  url "https://github.com/ollama/ollama/releases/download/v0.6.8/Ollama-darwin.zip"
   name "ollama"
   desc "ollama - get up and running with Llama 2, Mistral, and other large language models locally"
   homepage "https://github.com/ollama/ollama"
-  sha256 "8344ad8b1c645f5169e8ba431174f3b64ca358ae25e1edd3c57fc8974e7d9d98"
+  sha256 "ba86af4807838b61ca76223ba08eae843b86b152a15105597549c85af99f5947"
 
   auto_updates true
 
