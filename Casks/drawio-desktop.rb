9eb7fe08a9cd56c087ffe1b866ee2185750b7379
GitHub Action <action@github.com>
2025-05-14 01:56:44 +0000

    Automated update

--- Casks/drawio-desktop.rb
+++ Casks/drawio-desktop.rb
@@ -1,11 +1,11 @@
 cask "drawio-desktop" do
-  version "v27.0.2"
+  version "v27.0.5"
 
-  url "https://github.com/jgraph/drawio-desktop/releases/download/v27.0.2/draw.io-universal-27.0.2.dmg"
+  url "https://github.com/jgraph/drawio-desktop/releases/download/v27.0.5/draw.io-universal-27.0.5.dmg"
   name "drawio-desktop"
   desc "Official electron build of draw.io"
   homepage "https://github.com/jgraph/drawio-desktop"
-  sha256 "4800ba7a0b76ddb4f56b37ab9ef48996ac3b772677bac63db9db9351374b0140"
+  sha256 "0942410cf7fee047b1aef9eda646a1f14ec3c910e155f2e84e742ea08a988f8a"
 
   auto_updates true
 
--- Casks/super-productivity.rb
+++ Casks/super-productivity.rb
@@ -1,11 +1,11 @@
 cask "super-productivity" do
-  version "v12.0.2"
+  version "v13.0.1"
 
-  url "https://github.com/johannesjo/super-productivity/releases/download/v12.0.2/superProductivity-universal.dmg"
+  url "https://github.com/johannesjo/super-productivity/releases/download/v13.0.1/superProductivity-universal.dmg"
   name "super-productivity"
   desc "Super Productivity is an advanced todo list app with integrated Timeboxing and time tracking capabilities. It also comes with integrations for Jira, Gitlab, GitHub and Open Project."
   homepage "https://github.com/johannesjo/super-productivity"
-  sha256 "ab727084df89dffecf55ece0e3f7dbac46c8e05f473040e26699633fc5947045"
+  sha256 "8269569196e741255bf3065eeb81f7aa6f9a581e87fc2dc0a90ca2477b4f68f4"
 
   auto_updates true
 
