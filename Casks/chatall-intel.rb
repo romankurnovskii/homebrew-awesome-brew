4c5e58c868cff14a3df3c53b17b1bb1d01f17fd7
GitHub Action <action@github.com>
2025-05-23 01:52:39 +0000

    Automated update

--- Casks/chatall-intel.rb
+++ Casks/chatall-intel.rb
@@ -1,11 +1,11 @@
 cask "chatall-intel" do
-  version "v1.84.109"
+  version "v1.85.110"
 
-  url "https://github.com/ai-shifu/ChatALL/releases/download/v1.84.109/ChatALL-1.84.109-mac-x64.dmg"
+  url "https://github.com/ai-shifu/ChatALL/releases/download/v1.85.110/ChatALL-1.85.110-mac-x64.dmg"
   name "ChatALL-intel"
   desc "Concurrently chat with ChatGPT, Bing Chat, bard, Alpaca, Vincuna, Claude, ChatGLM, MOSS, iFlytek Spark, ERNIE and more, discover the best answers"
   homepage "https://github.com/sunner/ChatALL"
-  sha256 "17d5c6863d872607bc3bb7e4759b3b1cacf9277739c6b5eefffec9fc077724f8"
+  sha256 "8fe8ab4efc0d1a725e79765ad7b9a53b48be0432f96b3791f8e9beedd5cecf08"
 
   auto_updates true
 
