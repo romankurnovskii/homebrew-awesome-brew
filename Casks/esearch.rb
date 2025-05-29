17e28b24973826e65bc772280c6b9e51e7699173
GitHub Action <action@github.com>
2025-05-29 01:53:41 +0000

    Automated update

--- Casks/esearch.rb
+++ Casks/esearch.rb
@@ -1,11 +1,11 @@
 cask "esearch" do
-  version "14.7.3"
+  version "14.7.4"
 
-  url "https://github.com/xushengfeng/eSearch/releases/download/14.7.3/eSearch-14.7.3-darwin-x64.dmg"
+  url "https://github.com/xushengfeng/eSearch/releases/download/14.7.4/eSearch-14.7.4-darwin-x64.dmg"
   name "eSearch"
   desc "Screenshot OCR search translate search for picture paste the picture on the screen screen recorder"
   homepage "https://github.com/xushengfeng/eSearch"
-  sha256 "bd7f099ce62a405f46c9dcae8fe2361446db4e962cac824beecacb323cdb134e"
+  sha256 "be5b8d914da2eb3c70c75cc2bc53a86bbb8828fcf33b65564c03aca4960a8a61"
 
   auto_updates true
 
--- Casks/onlook-arm.rb
+++ Casks/onlook-arm.rb
@@ -1,11 +1,11 @@
 cask "onlook-arm" do
-  version "v0.2.29"
+  version "v0.2.30"
 
-  url "https://github.com/onlook-dev/onlook/releases/download/v0.2.29/Onlook-arm64.dmg"
+  url "https://github.com/onlook-dev/onlook/releases/download/v0.2.30/Onlook-arm64.dmg"
   name "onlook-arm"
   desc "Cursor for Designers. Design directly in your live React app and publish your changes to code onlook.com"
   homepage "https://github.com/onlook-dev/onlook"
-  sha256 "629d4f90ee8b89058ba164734216f79aea545b46095176fa1015806350da4eca"
+  sha256 "d209e137a8241b0b21c2b160af88484a69a40bc21f048e1946fefced13d425fc"
 
   auto_updates true
 
