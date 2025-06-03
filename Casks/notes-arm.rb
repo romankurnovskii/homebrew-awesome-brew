7fa88c07d4fa04e2cc92043c9e0c2d7345e01449
GitHub Action <action@github.com>
2025-06-03 01:56:29 +0000

    Automated update

--- Casks/notes-arm.rb
+++ Casks/notes-arm.rb
@@ -1,11 +1,11 @@
 cask "notes-arm" do
-  version "v0.93.0"
+  version "v0.94.0"
 
-  url "https://github.com/TriliumNext/Notes/releases/download/v0.93.0/TriliumNextNotes-v0.93.0-macos-arm64.dmg"
+  url "https://github.com/TriliumNext/Notes/releases/download/v0.94.0/TriliumNextNotes-v0.94.0-macos-arm64.dmg"
   name "Notes-arm"
   desc "TriliumNext Notes - personal knowledge base"
   homepage "https://github.com/TriliumNext/Notes"
-  sha256 "9268ce38a584214b75f265b304968df1575480912ec3ce65e17f94dbccacad90"
+  sha256 "7e384342319f07670ec1a7a78578299def6b9eeb3823ee1a7245b8d39c6fbdc6"
 
   auto_updates true
 
