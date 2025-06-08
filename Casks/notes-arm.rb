e9e11f58f66ea2878eaed985e12a0ffc0cef07cf
GitHub Action <action@github.com>
2025-06-08 02:03:56 +0000

    Automated update

--- Casks/notes-arm.rb
+++ Casks/notes-arm.rb
@@ -1,11 +1,11 @@
 cask "notes-arm" do
-  version "v0.94.0"
+  version "v0.94.1"
 
-  url "https://github.com/TriliumNext/Notes/releases/download/v0.94.0/TriliumNextNotes-v0.94.0-macos-arm64.dmg"
+  url "https://github.com/TriliumNext/Notes/releases/download/v0.94.1/TriliumNextNotes-v0.94.1-macos-arm64.dmg"
   name "Notes-arm"
   desc "TriliumNext Notes - personal knowledge base"
   homepage "https://github.com/TriliumNext/Notes"
-  sha256 "7e384342319f07670ec1a7a78578299def6b9eeb3823ee1a7245b8d39c6fbdc6"
+  sha256 "1448281ab40042f1a2f3d8513f84439cef9cb8b466c554fe26904dabf7d2db61"
 
   auto_updates true
 
--- Casks/notes-intel.rb
+++ Casks/notes-intel.rb
@@ -1,11 +1,11 @@
 cask "notes-intel" do
-  version "v0.94.0"
+  version "v0.94.1"
 
-  url "https://github.com/TriliumNext/Notes/releases/download/v0.94.0/TriliumNextNotes-v0.94.0-macos-x64.dmg"
+  url "https://github.com/TriliumNext/Notes/releases/download/v0.94.1/TriliumNextNotes-v0.94.1-macos-x64.dmg"
   name "Notes-intel"
   desc "TriliumNext Notes - personal knowledge base"
   homepage "https://github.com/TriliumNext/Notes"
-  sha256 "2c63ae136b1fe5abc4effe2164a8d1fdb4d9a131740e8d10495b536c8a1d6a59"
+  sha256 "101d3bfc66912a31e05468c300e3ad1fa897c1011d8e13deacd73a56acee2d84"
 
   auto_updates true
 
