ea924d4abe23e6a44d7dd75737d012126be8df97
GitHub Action <action@github.com>
2025-06-09 02:02:29 +0000

    Automated update

--- Casks/pomodoro-intel.rb
+++ Casks/pomodoro-intel.rb
@@ -1,11 +1,11 @@
 cask "pomodoro-intel" do
-  version "v3.6.0"
+  version "v3.6.1"
 
-  url "https://github.com/G07cha/pomodoro/releases/download/v3.6.0/Pomodoro_3.6.0_x64.dmg"
+  url "https://github.com/G07cha/pomodoro/releases/download/v3.6.1/Pomodoro_3.6.1_x64.dmg"
   name "pomodoro-intel"
   desc "Pomodoro time managment tool build with Tauri"
   homepage "https://github.com/G07cha/pomodoro"
-  sha256 "a5f599c6b9275cc9a6f4e222abf2aebed69bd36efd8a6db0c341ab9c717bf239"
+  sha256 "ab770818fe3e280b7cfc0bb7cec4d73e1ac9da7241fc60278abcc0d1528fa3d7"
 
   auto_updates true
 
