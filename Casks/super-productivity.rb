9c042959e1bc850dc214c848a8737fe65a6e671d
GitHub Action <action@github.com>
2025-06-22 02:05:54 +0000

    Automated update

--- Casks/super-productivity.rb
+++ Casks/super-productivity.rb
@@ -1,11 +1,11 @@
 cask "super-productivity" do
-  version "v13.1.0"
+  version "v13.1.2"
 
-  url "https://github.com/johannesjo/super-productivity/releases/download/v13.1.0/superProductivity-universal.dmg"
+  url "https://github.com/johannesjo/super-productivity/releases/download/v13.1.2/superProductivity-universal.dmg"
   name "super-productivity"
   desc "Super Productivity is an advanced todo list app with integrated Timeboxing and time tracking capabilities. It also comes with integrations for Jira, Gitlab, GitHub and Open Project."
   homepage "https://github.com/johannesjo/super-productivity"
-  sha256 "5f57e7f86417fa5d1a04a7fc07fb95da60a997a4b74378dc2fa2487a6f69a950"
+  sha256 "6a5de6e72e9565bdb5e336607e041806bf3ba407a192ebe01c53f38d78abef43"
 
   auto_updates true
 
--- Casks/trilium.rb
+++ Casks/trilium.rb
@@ -1,11 +1,11 @@
 cask "trilium" do
-  version "v0.63.7"
+  version "v0.95.0"
 
-  url "https://github.com/TriliumNext/trilium/releases/download/v0.63.7/trilium-mac-x64-0.63.7.zip"
+  url "https://github.com/TriliumNext/trilium/releases/download/v0.95.0/TriliumNextNotes-v0.95.0-macos-arm64.zip"
   name "trilium"
   desc "Build your personal knowledge base with Trilium Notes"
   homepage "https://github.com/zadam/trilium"
-  sha256 "c110cd7d6fdcdfec9d833937f09e611af967786e3fa838673d6274a7268b0b22"
+  sha256 "fa96327e27fde281cb8143bbaf7fd218d8d753d3b144643b8d776ad2b243686c"
 
   auto_updates true
 
