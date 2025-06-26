1cf9c94d0c0eb43f41ef2252d5d639807a71e2ff
GitHub Action <action@github.com>
2025-06-26 01:57:42 +0000

    Automated update

--- Casks/alspotron-arm.rb
+++ Casks/alspotron-arm.rb
@@ -1,11 +1,11 @@
 cask "alspotron-arm" do
-  version "v0.26.0"
+  version "v0.26.1"
 
-  url "https://github.com/organization/lyrs/releases/download/v0.26.0/Lyrs-0.26.0-arm64.dmg"
+  url "https://github.com/organization/lyrs/releases/download/v0.26.1/Lyrs-0.26.1-arm64.dmg"
   name "alspotron-arm"
   desc "Alspotron: Shows the egg lyrics of the track, playing on Spotify / YouTube Music"
   homepage "https://github.com/organization/alspotron"
-  sha256 "c6ea7cc29ac76a54606079711c86381a9cf49e7cab98081d46e7303c3b2ec582"
+  sha256 "ce52aaa0519952149e5c591e2a976a7bafcf38b40c37b56850cab27517c7e53e"
 
   auto_updates true
 
