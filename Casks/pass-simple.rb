cd8265638fb0d3075ce5246583989c0abbe166bb
GitHub Action <action@github.com>
2025-07-08 07:17:10 +0000

    Automated update

---
+++ Casks/pass-simple.rb
@@ -0,0 +1,24 @@
+cask "pass-simple" do
+  arch arm: "arm64", intel: "x86_64"
+  version "1.0.22"
+  sha256  arm: "fd9ab8e2b43219f226b7ec5ead06ad61117350187f1672a98bdc77271126e422",
+          intel: "137974d4f64e658e3e4488acaf1ae65116a23b92be1c17689b523062461cf5fb"
+
+  url "https://downloads.sourceforge.net/pass-simple/pass-simple-#{version}-#{arch}.dmg"
+  name "Pass simple"
+  desc "GUI for pass cli util"
+  homepage "https://sourceforge.net/projects/pass-simple/"
+
+  depends_on macos: ">= :big_sur"
+  depends_on formula: "pass"
+  depends_on formula: "pinentry-mac"
+  depends_on formula: "botan"
+  depends_on formula: "json-c"  
+
+  app "pass-simple.app"
+
+  zap trash: [
+    "~/Library/Caches/Pass simple",
+    "~/Library/Preferences/com.shemeshg.PassSimple.plist",
+  ]
+end
