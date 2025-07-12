6cb5b9ea9f605903512589825446f80010d75c63
GitHub Action <action@github.com>
2025-07-12 02:06:35 +0000

    Automated update

--- Casks/dnschanger-desktop-arm.rb
+++ Casks/dnschanger-desktop-arm.rb
@@ -1,11 +1,11 @@
 cask "dnschanger-desktop-arm" do
-  version "v2.3.4"
+  version "v2.3.5"
 
-  url "https://github.com/DnsChanger/dnsChanger-desktop/releases/download/v2.3.4/DNS-Changer-mac-arm64-2.3.4.dmg"
+  url "https://github.com/DnsChanger/dnsChanger-desktop/releases/download/v2.3.5/DNS-Changer-mac-arm64-2.3.5.dmg"
   name "dnsChanger-desktop-arm"
   desc "DNS Changer for Windows, Linux, Mac operating systems"
   homepage "https://github.com/DnsChanger/dnsChanger-desktop"
-  sha256 "9426a30a16cac7cf7cf7842cadb39097ad2e42c16973f6a2345495f3e6c33414"
+  sha256 "71365bc7212517ab158304676343317a1c100ccb61cdca1cbbfca0186f99728b"
 
   auto_updates true
 
