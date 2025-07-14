eb282a94b0e6c5a55b6cb78a0ebad8067f05e0a6
GitHub Action <action@github.com>
2025-07-14 02:07:47 +0000

    Automated update

--- Casks/youtube-music-intel.rb
+++ Casks/youtube-music-intel.rb
@@ -1,11 +1,11 @@
 cask "youtube-music-intel" do
-  version "v3.9.0"
+  version "v3.10.0"
 
-  url "https://github.com/th-ch/youtube-music/releases/download/v3.9.0/YouTube-Music-3.9.0.dmg"
+  url "https://github.com/th-ch/youtube-music/releases/download/v3.10.0/YouTube-Music-3.10.0.dmg"
   name "youtube-music-intel"
   desc "YouTube Music Desktop / built-in ad blocker / downloader"
   homepage "https://github.com/th-ch/youtube-music"
-  sha256 "7ba47ff3941f6d0caaf9dea9a6fa48cc4944f387b5ec44a75f0cb07455843c75"
+  sha256 "36b3eeccf9f8545517aebd5490db7fb392c57006de27a770937e1c260755380d"
 
   auto_updates true
 
