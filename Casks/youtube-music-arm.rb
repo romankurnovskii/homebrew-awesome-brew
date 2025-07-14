f459ff420486147acdad2ccbbdf1056fa43dd9db
GitHub Action <action@github.com>
2025-07-14 02:07:47 +0000

    Automated update

--- Casks/youtube-music-arm.rb
+++ Casks/youtube-music-arm.rb
@@ -1,11 +1,11 @@
 cask "youtube-music-arm" do
-  version "v3.9.0"
+  version "v3.10.0"
 
-  url "https://github.com/th-ch/youtube-music/releases/download/v3.9.0/YouTube-Music-3.9.0-arm64.dmg"
+  url "https://github.com/th-ch/youtube-music/releases/download/v3.10.0/YouTube-Music-3.10.0-arm64.dmg"
   name "youtube-music-arm"
   desc "YouTube Music Desktop / built-in ad blocker / downloader"
   homepage "https://github.com/th-ch/youtube-music"
-  sha256 "76ab9ae2c30f37628ec1580f0b530cd56875a1a40335192257d388c18c6c613f"
+  sha256 "ee30ccf7be62be04e17d94d54f0eedcfc74ee4048cde15fd9c9540ece1cecaf2"
 
   auto_updates true
 
