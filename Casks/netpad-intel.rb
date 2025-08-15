ab70762d8b6839d28b0803ab515badd4842e2308
GitHub Action <action@github.com>
2025-08-15 01:59:04 +0000

    Automated update

--- Casks/netpad-intel.rb
+++ Casks/netpad-intel.rb
@@ -1,11 +1,11 @@
 cask "netpad-intel" do
-  version "v0.9.1"
+  version "v0.10.0"
 
-  url "https://github.com/tareqimbasher/NetPad/releases/download/v0.9.1/netpad-0.9.1-mac-x64.zip"
+  url "https://github.com/tareqimbasher/NetPad/releases/download/v0.10.0/netpad-0.10.0-mac-x64.zip"
   name "NetPad-intel"
   desc "Robotics visualization and debugging"
   homepage "https://github.com/tareqimbasher/NetPad"
-  sha256 "543bc73cab9b75a20116242de068433341c4a745acef8dbc96e1bb893c3a421d"
+  sha256 "1ee83b08ea21143c0e238591693879b2f41a1d5128349a79217983a409f289a7"
 
   auto_updates true
 
