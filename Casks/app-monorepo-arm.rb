aabfdf3ff0c81081978cfb56dd177dfdf33f0913
GitHub Action <action@github.com>
2025-08-30 01:43:24 +0000

    Automated update

--- Casks/app-monorepo-arm.rb
+++ Casks/app-monorepo-arm.rb
@@ -1,11 +1,11 @@
 cask "app-monorepo-arm" do
-  version "v5.12.0"
+  version "v5.12.1"
 
-  url "https://github.com/OneKeyHQ/app-monorepo/releases/download/v5.12.0/OneKey-Wallet-5.12.0-mac-arm64.dmg"
+  url "https://github.com/OneKeyHQ/app-monorepo/releases/download/v5.12.1/OneKey-Wallet-5.12.1-mac-arm64.dmg"
   name "app-monorepo-arm"
   desc "Open source and community driven crypto wallet"
   homepage "https://github.com/OneKeyHQ/app-monorepo"
-  sha256 "a9c6b56ef4919ec37326e4688fe1741c1c4242293203e0bf8ecd50bf27a71691"
+  sha256 "01e40c628963d28fe89a593a9053b6235ee5e8079a87d5b443434c210aa6e1db"
 
   auto_updates true
 
--- Casks/super-productivity.rb
+++ Casks/super-productivity.rb
@@ -1,11 +1,11 @@
 cask "super-productivity" do
-  version "v14.3.4"
+  version "v14.4.0"
 
-  url "https://github.com/johannesjo/super-productivity/releases/download/v14.3.4/superProductivity-universal.dmg"
+  url "https://github.com/johannesjo/super-productivity/releases/download/v14.4.0/superProductivity-universal.dmg"
   name "super-productivity"
   desc "Super Productivity is an advanced todo list app with integrated Timeboxing and time tracking capabilities. It also comes with integrations for Jira, Gitlab, GitHub and Open Project."
   homepage "https://github.com/johannesjo/super-productivity"
-  sha256 "dff52a1ba8ed9e72571062113e0115625f8050c91dfbdb3d9cf80afa53a70784"
+  sha256 "9f5ce1093a592bbe42efc2855f4c57e43ee5f760a6fdf13afd1d6c80f50cd855"
 
   auto_updates true
 
