2efe07963ead178ebd0a82c2441072ea9ef80755
GitHub Action <action@github.com>
2025-08-30 01:43:24 +0000

    Automated update

--- Casks/app-monorepo-intel.rb
+++ Casks/app-monorepo-intel.rb
@@ -1,11 +1,11 @@
 cask "app-monorepo-intel" do
-  version "v5.12.0"
+  version "v5.12.1"
 
-  url "https://github.com/OneKeyHQ/app-monorepo/releases/download/v5.12.0/OneKey-Wallet-5.12.0-mac-x64.dmg"
+  url "https://github.com/OneKeyHQ/app-monorepo/releases/download/v5.12.1/OneKey-Wallet-5.12.1-mac-x64.dmg"
   name "app-monorepo-intel"
   desc "Open source and community driven crypto wallet"
   homepage "https://github.com/OneKeyHQ/app-monorepo"
-  sha256 "c7cff760b46fcab88bd85520dcf2f212c3c497dcf9d9bacf6dcb1d5f7507ebbc"
+  sha256 "0659146a015f509cb09e03f009eca1705cafebb09356a12e37463708657544e4"
 
   auto_updates true
 
