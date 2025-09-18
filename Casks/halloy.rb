04dec75ee3f438076be68b3eb6c7555a162f13e1
GitHub Action <action@github.com>
2025-09-18 01:47:15 +0000

    Automated update

--- Casks/halloy.rb
+++ Casks/halloy.rb
@@ -1,11 +1,11 @@
 cask "halloy" do
-  version "2025.8"
+  version "2025.9"
 
-  url "https://github.com/squidowl/halloy/releases/download/2025.8/halloy.dmg"
+  url "https://github.com/squidowl/halloy/releases/download/2025.9/halloy.dmg"
   name "halloy"
   desc "IRC application written in Rust"
   homepage "https://github.com/squidowl/halloy"
-  sha256 "6fe503ed47270864c04275c22112fc6606179eb107b2899a9b6127ba3eb38c36"
+  sha256 "2cd7c3af9ceae25571346a08cd1a41a9d32b9d4156691168abdfe92860aed010"
 
   auto_updates true
 
--- Casks/keyboardcowboy.rb
+++ Casks/keyboardcowboy.rb
@@ -1,11 +1,11 @@
 cask "keyboardcowboy" do
-  version "3.27.3"
+  version "3.28.0"
 
-  url "https://github.com/zenangst/KeyboardCowboy/releases/download/3.27.3/Keyboard.Cowboy.3.27.3.dmg"
+  url "https://github.com/zenangst/KeyboardCowboy/releases/download/3.28.0/Keyboard.Cowboy.3.28.0.dmg"
   name "KeyboardCowboy"
   desc "Keyboard shortcut utility."
   homepage "https://github.com/zenangst/KeyboardCowboy"
-  sha256 "c7b2695cec0a9825a5a7ca76170d784992b4a11433a1f680b17a6a1449e61671"
+  sha256 "e1b05a93bdc3a9d6868a5985d7b168ae8e218d8a73dca8ee83ddfeff7214e918"
 
   auto_updates true
 
