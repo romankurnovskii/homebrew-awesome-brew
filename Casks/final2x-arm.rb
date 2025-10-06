26ec33cd76aa4bd426ba14d7d685e4f051b266ff
GitHub Action <action@github.com>
2025-10-06 01:46:21 +0000

    Automated update

--- Casks/final2x-arm.rb
+++ Casks/final2x-arm.rb
@@ -1,11 +1,11 @@
 cask "final2x-arm" do
-  version "2025-08-16"
+  version "v4.0.0"
 
-  url "https://github.com/EutropicAI/Final2x/releases/download/2025-08-16/Final2x-macos-arm64-dmg.dmg"
+  url "https://github.com/EutropicAI/Final2x/releases/download/v4.0.0/Final2x-macos-arm64-dmg.dmg"
   name "Final2x-arm"
   desc "2^x Image Super-Resolution"
   homepage "https://github.com/Tohrusky/Final2x"
-  sha256 "a1e90f74c00c966b5749e533917027175c5a112cd319425419a3666b15a9f9cd"
+  sha256 "6d2a03de4a0e6e4683a4aa6d4f977cc10807c4f796c1e4a45d21a5184b8740f5"
 
   auto_updates true
 
