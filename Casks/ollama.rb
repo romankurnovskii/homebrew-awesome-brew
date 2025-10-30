8ac9e53a885159cda53bab09377c1996a0c63888
GitHub Action <action@github.com>
2025-10-30 01:58:42 +0000

    Automated update

--- Casks/ollama.rb
+++ Casks/ollama.rb
@@ -1,11 +1,11 @@
 cask "ollama" do
-  version "v0.12.6"
+  version "v0.12.7"
 
-  url "https://github.com/ollama/ollama/releases/download/v0.12.6/Ollama-darwin.zip"
+  url "https://github.com/ollama/ollama/releases/download/v0.12.7/Ollama-darwin.zip"
   name "ollama"
   desc "ollama - get up and running with Llama 2, Mistral, and other large language models locally"
   homepage "https://github.com/ollama/ollama"
-  sha256 "eed35955ec5f41f3d1488aa2486c2d7ff48ca3842e5771cc0daa3496d4224566"
+  sha256 "3651c50093c37e00aef78cb0480abf02b608b89556b56eb4c4913365c195e226"
 
   auto_updates true
 
--- Casks/photos-desktop.rb
+++ Casks/photos-desktop.rb
@@ -1,11 +1,11 @@
 cask "photos-desktop" do
-  version "v1.7.14"
+  version "v1.7.15"
 
-  url "https://github.com/ente-io/photos-desktop/releases/download/v1.7.14/ente-1.7.14-universal.dmg"
+  url "https://github.com/ente-io/photos-desktop/releases/download/v1.7.15/ente-1.7.15-universal.dmg"
   name "photos-desktop"
   desc "Desktop app for ente Photos"
   homepage "https://github.com/ente-io/photos-desktop"
-  sha256 "4df8b45114e4e07ad42650443c867e483431938fad457f768934fd125dac6cb2"
+  sha256 "b8993e5e2ef3b9dd9a0476b8c5b9e42a30a6f24dbca41b215a6f501a82f34bca"
 
   auto_updates true
 
