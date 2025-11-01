edeb2b8bb511eb758ee6346947aafd03f36b6e57
GitHub Action <action@github.com>
2025-11-01 01:54:59 +0000

    Automated update

--- Casks/app-monorepo-arm.rb
+++ Casks/app-monorepo-arm.rb
@@ -1,11 +1,11 @@
 cask "app-monorepo-arm" do
-  version "v5.15.0"
+  version "v5.16.0"
 
-  url "https://github.com/OneKeyHQ/app-monorepo/releases/download/v5.15.0/OneKey-Wallet-5.15.0-mac-arm64.dmg"
+  url "https://github.com/OneKeyHQ/app-monorepo/releases/download/v5.16.0/OneKey-Wallet-5.16.0-mac-arm64.dmg"
   name "app-monorepo-arm"
   desc "Open source and community driven crypto wallet"
   homepage "https://github.com/OneKeyHQ/app-monorepo"
-  sha256 "65ec59cac3862541c6fb7fdd7e6e5c42dd5c40863984bf80eebb7eab97327664"
+  sha256 "8d39800c3d3a6dfa33261ad19572affb9a777be014a9f5d2241a1bf3e7431d52"
 
   auto_updates true
 
--- Casks/app-monorepo-intel.rb
+++ Casks/app-monorepo-intel.rb
@@ -1,11 +1,11 @@
 cask "app-monorepo-intel" do
-  version "v5.15.0"
+  version "v5.16.0"
 
-  url "https://github.com/OneKeyHQ/app-monorepo/releases/download/v5.15.0/OneKey-Wallet-5.15.0-mac-x64.dmg"
+  url "https://github.com/OneKeyHQ/app-monorepo/releases/download/v5.16.0/OneKey-Wallet-5.16.0-mac-x64.dmg"
   name "app-monorepo-intel"
   desc "Open source and community driven crypto wallet"
   homepage "https://github.com/OneKeyHQ/app-monorepo"
-  sha256 "54bfca3a65e1ad722ce5052be179eb94e5930e88714b3031427a50823a3a2be4"
+  sha256 "b51be5a82ae81785b860a0bf6ff7542d5e19a955ee5a6d22d24d351d0990b4d2"
 
   auto_updates true
 
