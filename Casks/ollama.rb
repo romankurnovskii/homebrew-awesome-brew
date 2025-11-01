f287d7c8bce0f3e903c1f1a02b708955c3be76c9
GitHub Action <action@github.com>
2025-11-01 01:54:59 +0000

    Automated update

--- Casks/ollama.rb
+++ Casks/ollama.rb
@@ -1,11 +1,11 @@
 cask "ollama" do
-  version "v0.12.7"
+  version "v0.12.8"
 
-  url "https://github.com/ollama/ollama/releases/download/v0.12.7/Ollama-darwin.zip"
+  url "https://github.com/ollama/ollama/releases/download/v0.12.8/Ollama-darwin.zip"
   name "ollama"
   desc "ollama - get up and running with Llama 2, Mistral, and other large language models locally"
   homepage "https://github.com/ollama/ollama"
-  sha256 "3651c50093c37e00aef78cb0480abf02b608b89556b56eb4c4913365c195e226"
+  sha256 "722977957b54a9b73587c42f32f97930cd73111214aea0f02e25aa412cc15d3c"
 
   auto_updates true
 
--- Casks/super-productivity.rb
+++ Casks/super-productivity.rb
@@ -1,11 +1,11 @@
 cask "super-productivity" do
-  version "v16.1.0"
+  version "v16.2.0"
 
-  url "https://github.com/johannesjo/super-productivity/releases/download/v16.1.0/superProductivity-universal.dmg"
+  url "https://github.com/johannesjo/super-productivity/releases/download/v16.2.0/superProductivity-universal.dmg"
   name "super-productivity"
   desc "Super Productivity is an advanced todo list app with integrated Timeboxing and time tracking capabilities. It also comes with integrations for Jira, Gitlab, GitHub and Open Project."
   homepage "https://github.com/johannesjo/super-productivity"
-  sha256 "9e447b37595e685a2a3f1d4b9d96fe0f0bc57e6509e0d1f3917613d4c23e51d6"
+  sha256 "446ffafc04c03fec09fa5ab64c19aeebb22f1c5207c001071725c121967bafbd"
 
   auto_updates true
 
