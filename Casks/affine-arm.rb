8b77d50a781a8fb68fd753b8125e1579f486a0de
GitHub Action <action@github.com>
2025-11-15 01:53:42 +0000

    Automated update

--- Casks/affine-arm.rb
+++ Casks/affine-arm.rb
@@ -1,11 +1,11 @@
 cask "affine-arm" do
-  version "v0.25.1"
+  version "v0.25.4"
 
-  url "https://github.com/toeverything/AFFiNE/releases/download/v0.25.1/affine-0.25.1-stable-macos-arm64.dmg"
+  url "https://github.com/toeverything/AFFiNE/releases/download/v0.25.4/affine-0.25.4-stable-macos-arm64.dmg"
   name "AFFiNE-arm"
   desc "Knowledge base that brings planning, sorting and creating all together. Notion and Miro."
   homepage "https://github.com/toeverything/AFFiNE"
-  sha256 "de56ad058f1870a63320ff8bae17c56c89e90d9bae2d6e094e1b14906aa82f55"
+  sha256 "a018354ef3cc9fd8c413c95075f1633518af6e769a0ef3764c9be28a1ce80831"
 
   auto_updates true
 
--- Casks/affine-intel.rb
+++ Casks/affine-intel.rb
@@ -1,11 +1,11 @@
 cask "affine-intel" do
-  version "v0.25.1"
+  version "v0.25.4"
 
-  url "https://github.com/toeverything/AFFiNE/releases/download/v0.25.1/affine-0.25.1-stable-macos-x64.dmg"
+  url "https://github.com/toeverything/AFFiNE/releases/download/v0.25.4/affine-0.25.4-stable-macos-x64.dmg"
   name "AFFiNE-intel"
   desc "Knowledge base that brings planning, sorting and creating all together. Notion and Miro."
   homepage "https://github.com/toeverything/AFFiNE"
-  sha256 "5189723ac2b565950e5059a2ad5fd6575ef76cc6596c2f3db3843d3c2426f980"
+  sha256 "d3aa15dfb00be4725674919bcfc7c3cded9bc2c5df787fb9f55a34704cfcf210"
 
   auto_updates true
 
