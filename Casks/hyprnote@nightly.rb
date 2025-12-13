01b971012b1630bfd88a27b3cd41918034fb4475
GitHub Action <action@github.com>
2025-12-13 01:55:11 +0000

    Automated update

--- Casks/hyprnote@nightly.rb
+++ Casks/hyprnote@nightly.rb
@@ -1,19 +1,22 @@
 cask "hyprnote@nightly" do
   arch arm: "aarch64", intel: "x86_64"
-  version :latest
-  sha256 :no_check
-  
-  url "https://desktop.hyprnote.com/download/latest/dmg-#{arch}?channel=nightly"
+
+  on_arm do
+    version "0.0.0"
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  end
+  on_intel do
+    version "0.0.0"
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  end
+
+  url "https://desktop2.hyprnote.com/download/#{version}/dmg-#{arch}?channel=nightly"
   name "Hyprnote Nightly"
   desc "Nightly build of Hyprnote application"
   homepage "https://github.com/fastrepl/hyprnote"
-  
+
   auto_updates true
   depends_on macos: ">= :sonoma"
-  
+
   app "Hyprnote Nightly.app"
-  
-  zap trash: [
-    "~/Library/Application Support/com.hyprnote.nightly",
-  ]
 end
