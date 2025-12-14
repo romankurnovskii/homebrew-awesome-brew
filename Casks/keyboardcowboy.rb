a8f7739218be6499c5edbb4d1fac77eebfa4478f
GitHub Action <action@github.com>
2025-12-14 02:08:02 +0000

    Automated update

--- Casks/keyboardcowboy.rb
+++ Casks/keyboardcowboy.rb
@@ -1,11 +1,11 @@
 cask "keyboardcowboy" do
-  version "3.28.3"
+  version "3.28.4"
 
-  url "https://github.com/zenangst/KeyboardCowboy/releases/download/3.28.3/Keyboard.Cowboy.3.28.3.dmg"
+  url "https://github.com/zenangst/KeyboardCowboy/releases/download/3.28.4/Keyboard.Cowboy.3.28.4.dmg"
   name "KeyboardCowboy"
   desc "Keyboard shortcut utility."
   homepage "https://github.com/zenangst/KeyboardCowboy"
-  sha256 "be7ebac346857ed6cf4eb44b783928780f35e8449c3de7728944ca48ddb3bd63"
+  sha256 "e95e4cc8fd5438215da659acfe20cd901081edcf7aa99824b6fc4001d5b5fc52"
 
   auto_updates true
 
