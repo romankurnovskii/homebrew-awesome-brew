6526f541b08d85721c564526fb82749e8d30dd65
GitHub Action <action@github.com>
2025-12-24 02:01:50 +0000

    Automated update

--- Casks/picgo-intel.rb
+++ Casks/picgo-intel.rb
@@ -1,11 +1,11 @@
 cask "picgo-intel" do
-  version "v2.4.0"
+  version "v2.4.1"
 
-  url "https://github.com/Molunerfinn/PicGo/releases/download/v2.4.0/PicGo-2.4.0-x64.dmg"
+  url "https://github.com/Molunerfinn/PicGo/releases/download/v2.4.1/PicGo-2.4.1-x64.dmg"
   name "PicGo-intel"
   desc "A simple & beautiful tool for pictures uploading built by vue-cli-electron-builder"
   homepage "https://github.com/Molunerfinn/PicGo"
-  sha256 "f7d065fbf762609a0d08681400011f4eb3521372c80db99b49c43e05b915f014"
+  sha256 "35f8ca140e791a9c7bb85319a8a906716cf842e78d1e3045caa267b0cba53c52"
 
   auto_updates true
 
