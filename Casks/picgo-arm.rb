7f03badfb0e1551c96ec859d0e57459e7787f546
GitHub Action <action@github.com>
2025-12-24 02:01:50 +0000

    Automated update

--- Casks/picgo-arm.rb
+++ Casks/picgo-arm.rb
@@ -1,11 +1,11 @@
 cask "picgo-arm" do
-  version "v2.4.0"
+  version "v2.4.1"
 
-  url "https://github.com/Molunerfinn/PicGo/releases/download/v2.4.0/PicGo-2.4.0-arm64.dmg"
+  url "https://github.com/Molunerfinn/PicGo/releases/download/v2.4.1/PicGo-2.4.1-arm64.dmg"
   name "PicGo-arm"
   desc "A simple & beautiful tool for pictures uploading built by vue-cli-electron-builder"
   homepage "https://github.com/Molunerfinn/PicGo"
-  sha256 "640dd021a93ecbe782d3a16517aad7c32c03fbfa23b9725da7e27372d680cf06"
+  sha256 "7232d7c6d5ee8ee7291d1c17aa2c13e19e942186864a1f120305ef43a38f6e2f"
 
   auto_updates true
 
