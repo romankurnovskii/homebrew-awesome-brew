16e82d9b0fb8f05a7f5777dfdcbdec76bbfc81f1
GitHub Action <action@github.com>
2026-01-01 02:14:30 +0000

    Automated update

--- Casks/desktop.rb
+++ Casks/desktop.rb
@@ -1,11 +1,11 @@
 cask "desktop" do
-  version "v1.15.0"
+  version "v1.15.1"
 
-  url "https://github.com/TurboWarp/desktop/releases/download/v1.15.0/TurboWarp-Setup-1.15.0.dmg"
+  url "https://github.com/TurboWarp/desktop/releases/download/v1.15.1/TurboWarp-Setup-1.15.1.dmg"
   name "desktop"
   desc "Offline editor for Scratch 3"
   homepage "https://github.com/TurboWarp/desktop"
-  sha256 "0d07ea8c3a61425b0875ef8567b84e02b9d4799ed4400a870f98d995b1808289"
+  sha256 "51b87430ded647d7a1b170bd21fbc902891780235dea24193b322f85088ae0e0"
 
   auto_updates true
 
