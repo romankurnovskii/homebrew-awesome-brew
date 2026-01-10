a489374ddfe5bbe3f67a0cca2366f2379c083740
GitHub Action <action@github.com>
2026-01-10 02:04:45 +0000

    Automated update

--- Casks/drawio-desktop.rb
+++ Casks/drawio-desktop.rb
@@ -1,11 +1,11 @@
 cask "drawio-desktop" do
-  version "v29.2.9"
+  version "v29.3.0"
 
-  url "https://github.com/jgraph/drawio-desktop/releases/download/v29.2.9/draw.io-universal-29.2.9.dmg"
+  url "https://github.com/jgraph/drawio-desktop/releases/download/v29.3.0/draw.io-universal-29.3.0.dmg"
   name "drawio-desktop"
   desc "Official electron build of draw.io"
   homepage "https://github.com/jgraph/drawio-desktop"
-  sha256 "b99c3dd1ad1b2d92200dbf1600a2335aa20de0cc21962005f331b9ac12a194fb"
+  sha256 "af901e604d63a12e763e5de69029f38c8c611c8011d1b3752a3234ef5fa88462"
 
   auto_updates true
 
