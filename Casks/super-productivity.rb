c5820259b8a547e85cbb157129eb80c8080d6872
GitHub Action <action@github.com>
2026-01-31 02:26:17 +0000

    Automated update

--- Casks/super-productivity.rb
+++ Casks/super-productivity.rb
@@ -1,11 +1,11 @@
 cask "super-productivity" do
-  version "v17.0.11"
+  version "v17.0.12"
 
-  url "https://github.com/super-productivity/super-productivity/releases/download/v17.0.11/superProductivity-universal.dmg"
+  url "https://github.com/super-productivity/super-productivity/releases/download/v17.0.12/superProductivity-universal.dmg"
   name "super-productivity"
   desc "Super Productivity is an advanced todo list app with integrated Timeboxing and time tracking capabilities. It also comes with integrations for Jira, Gitlab, GitHub and Open Project."
   homepage "https://github.com/johannesjo/super-productivity"
-  sha256 "c411f45420d89a789e8b00687cb09ae17d647de57a66c2e033b7fbce33443d6a"
+  sha256 "d95bd3b0c45184571b3a27517662f3bea700a6eec86d7f28eb6c41eefed237e4"
 
   auto_updates true
 
