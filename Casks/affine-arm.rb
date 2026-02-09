02baa85c972dec9e2507d646612199d21a92a25e
GitHub Action <action@github.com>
2026-02-09 02:40:10 +0000

    Automated update

--- Casks/affine-arm.rb
+++ Casks/affine-arm.rb
@@ -1,11 +1,11 @@
 cask "affine-arm" do
-  version "v0.26.0"
+  version "v0.26.2"
 
-  url "https://github.com/toeverything/AFFiNE/releases/download/v0.26.0/affine-0.26.0-stable-macos-arm64.dmg"
+  url "https://github.com/toeverything/AFFiNE/releases/download/v0.26.2/affine-0.26.2-stable-macos-arm64.dmg"
   name "AFFiNE-arm"
   desc "Knowledge base that brings planning, sorting and creating all together. Notion and Miro."
   homepage "https://github.com/toeverything/AFFiNE"
-  sha256 "342ab632686d430f9fe258bc623719f5e2bb87130f9346dcd8acbd0caffae056"
+  sha256 "291f570ce0065b75c85386815b108a71afbc565ba14fa04affa96618fd89f8d0"
 
   auto_updates true
 
--- Casks/weakauras-companion.rb
+++ Casks/weakauras-companion.rb
@@ -1,11 +1,11 @@
 cask "weakauras-companion" do
-  version "v5.3.0"
+  version "v5.3.1"
 
-  url "https://github.com/WeakAuras/WeakAuras-Companion/releases/download/v5.3.0/WeakAuras-Companion-5.3.0-mac-universal.dmg"
+  url "https://github.com/WeakAuras/WeakAuras-Companion/releases/download/v5.3.1/WeakAuras-Companion-5.3.1-mac-universal.dmg"
   name "WeakAuras-Companion"
   desc "A cross-platform application built to provide the missing link between Wago.io and World of Warcraft"
   homepage "https://github.com/WeakAuras/WeakAuras-Companion"
-  sha256 "3be0f32128bccdfda14f41fddb7d34f576c99ef9919472279987fa0e30463f40"
+  sha256 "bc430a4d29679c9e446dfe1c215596a307438cb66b885ce637b4c3b22b1fd990"
 
   auto_updates true
 
