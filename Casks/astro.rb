dbbdda5742aa24c7f435001aadce7cb34eef3f66
GitHub Action <action@github.com>
2026-02-13 02:46:10 +0000

    Automated update

--- Casks/astro.rb
+++ Casks/astro.rb
@@ -1,11 +1,11 @@
 cask "astro" do
-  version "306"
+  version "307"
 
-  url "https://github.com/matteospada/astro/releases/download/306/Astro.dmg"
+  url "https://github.com/matteospada/astro/releases/download/307/Astro.dmg"
   name "astro"
   desc "Astro - App Store Optimization tool for iOS Developers | tryastro.app"
   homepage "https://github.com/matteospada/astro"
-  sha256 "420b61a8c4bb79f6cfb93db56a71159e39df5502f97892dc076deff50d595c53"
+  sha256 "ed71755dba6be26de96ad8eb7c725763c2cc5d7dab30943320f4794f95829c84"
 
   auto_updates true
 
