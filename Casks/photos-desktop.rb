ac2e62d9b3d09e32fee435b1e11cf8957fbe87e6
GitHub Action <action@github.com>
2026-03-06 02:33:31 +0000

    Automated update

--- Casks/photos-desktop.rb
+++ Casks/photos-desktop.rb
@@ -1,11 +1,11 @@
 cask "photos-desktop" do
-  version "v1.7.18"
+  version "v1.7.21"
 
-  url "https://github.com/ente-io/photos-desktop/releases/download/v1.7.18/ente-1.7.18-universal.dmg"
+  url "https://github.com/ente-io/photos-desktop/releases/download/v1.7.21/ente-1.7.21-universal.dmg"
   name "photos-desktop"
   desc "Desktop app for ente Photos"
   homepage "https://github.com/ente-io/photos-desktop"
-  sha256 "aa203001e7f39b821018a6645eaceda8b6deea8c4721ab1733b625b7033fc24f"
+  sha256 "7ff4416e15320289a2c85b65b78558671e3527e018b6c40ddb9148fbd2209d7f"
 
   auto_updates true
 
