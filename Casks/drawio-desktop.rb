8f8589e4b536dce720386e87d45f9736496b30b9
GitHub Action <action@github.com>
2026-03-09 02:36:33 +0000

    Automated update

--- Casks/drawio-desktop.rb
+++ Casks/drawio-desktop.rb
@@ -1,11 +1,11 @@
 cask "drawio-desktop" do
-  version "v29.5.2"
+  version "v29.6.1"
 
-  url "https://github.com/jgraph/drawio-desktop/releases/download/v29.5.2/draw.io-universal-29.5.2.dmg"
+  url "https://github.com/jgraph/drawio-desktop/releases/download/v29.6.1/draw.io-universal-29.6.1.dmg"
   name "drawio-desktop"
   desc "Official electron build of draw.io"
   homepage "https://github.com/jgraph/drawio-desktop"
-  sha256 "34ef7141bf4cd1e0b1e0a6767d8e0b444bde7b756cc4c35fb5dcb092ed9d655f"
+  sha256 "992d233aff3406a8060104a5dcb13804013a865f9c423fd42bdc43be21b963d2"
 
   auto_updates true
 
--- Casks/trilium.rb
+++ Casks/trilium.rb
@@ -1,11 +1,11 @@
 cask "trilium" do
-  version "v0.102.0"
+  version "v0.102.1"
 
-  url "https://github.com/TriliumNext/Trilium/releases/download/v0.102.0/TriliumNotes-v0.102.0-macos-arm64.zip"
+  url "https://github.com/TriliumNext/Trilium/releases/download/v0.102.1/TriliumNotes-v0.102.1-macos-arm64.zip"
   name "trilium"
   desc "Build your personal knowledge base with Trilium Notes"
   homepage "https://github.com/zadam/trilium"
-  sha256 "46bddd6b847feeb4f98a9a46137f1158e193f96d325e9e52e37328d6715dcccc"
+  sha256 "9c8a1d16fc8f23b2c9504e0f23504cc08dd79f3fce7f332b81a1c7925b5480ac"
 
   auto_updates true
 
