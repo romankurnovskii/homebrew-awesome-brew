8577ba585cfc0e42e67ac1aa0ed8b0ba258ecf08
GitHub Action <action@github.com>
2026-03-30 02:54:12 +0000

    Automated update

--- Casks/desktop.rb
+++ Casks/desktop.rb
@@ -1,11 +1,11 @@
 cask "desktop" do
-  version "v1.15.2"
+  version "v1.15.3"
 
-  url "https://github.com/TurboWarp/desktop/releases/download/v1.15.2/TurboWarp-Setup-1.15.2.dmg"
+  url "https://github.com/TurboWarp/desktop/releases/download/v1.15.3/TurboWarp-Setup-1.15.3.dmg"
   name "desktop"
   desc "Offline editor for Scratch 3"
   homepage "https://github.com/TurboWarp/desktop"
-  sha256 "8dd3aa832b41d2f19ebf617dc58c893f1b001fcee032cc9d8599b744156151a5"
+  sha256 "7ae399f36fd4fa1d9dbd042979e605489df058c35c29b73f1ace4f2e7509c8bb"
 
   auto_updates true
 
--- Casks/super-productivity.rb
+++ Casks/super-productivity.rb
@@ -1,11 +1,11 @@
 cask "super-productivity" do
-  version "v18.0.0"
+  version "v18.1.0"
 
-  url "https://github.com/super-productivity/super-productivity/releases/download/v18.0.0/superProductivity-universal.dmg"
+  url "https://github.com/super-productivity/super-productivity/releases/download/v18.1.0/superProductivity-universal.dmg"
   name "super-productivity"
   desc "Super Productivity is an advanced todo list app with integrated Timeboxing and time tracking capabilities. It also comes with integrations for Jira, Gitlab, GitHub and Open Project."
   homepage "https://github.com/johannesjo/super-productivity"
-  sha256 "4d6ed13dc7a61ad432489d8d1427031c7e1a6f15fa4f211e15846bfa14e2c350"
+  sha256 "5537efdfeaa580c0bdf8cbeef4bacc8a21cae98ace5e68eb8a1577f4c9c68884"
 
   auto_updates true
 
