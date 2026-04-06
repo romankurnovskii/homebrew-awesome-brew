b22a622a38c896bba90b3a5508b93ab8575549fe
GitHub Action <action@github.com>
2026-04-06 02:58:52 +0000

    Automated update

--- Casks/cpeditor.rb
+++ Casks/cpeditor.rb
@@ -1,11 +1,11 @@
 cask "cpeditor" do
-  version "6.11.2"
+  version "7.0.2"
 
-  url "https://github.com/cpeditor/cpeditor/releases/download/6.11.2/cpeditor-6.11.2-macos-x64.dmg"
+  url "https://github.com/cpeditor/cpeditor/releases/download/7.0.2/cpeditor-7.0.2-macos-x64.dmg"
   name "cpeditor"
   desc "The IDE for competitive programming 🎉 | Fetch, Code, Compile, Run, Check, Submit 🚀."
   homepage "https://github.com/cpeditor/cpeditor"
-  sha256 "bb475484fc07e3d42880306e22f4c475b1af66399e31dd55d9a7e36a61d3a60f"
+  sha256 "73cf6d030959a7f4951703d5d7b9a2e225731b72421eea90f29e824df0a20070"
 
   auto_updates true
 
--- Casks/trilium.rb
+++ Casks/trilium.rb
@@ -1,11 +1,11 @@
 cask "trilium" do
-  version "v0.102.1"
+  version "v0.102.2"
 
-  url "https://github.com/TriliumNext/Trilium/releases/download/v0.102.1/TriliumNotes-v0.102.1-macos-arm64.zip"
+  url "https://github.com/TriliumNext/Trilium/releases/download/v0.102.2/TriliumNotes-v0.102.2-macos-arm64.zip"
   name "trilium"
   desc "Build your personal knowledge base with Trilium Notes"
   homepage "https://github.com/zadam/trilium"
-  sha256 "9c8a1d16fc8f23b2c9504e0f23504cc08dd79f3fce7f332b81a1c7925b5480ac"
+  sha256 "1a08588d2ec49eff0841c352f19c085f86e673a89980678bd56e08bc218c881c"
 
   auto_updates true
 
