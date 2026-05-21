a05599664095692f5d791487d33ec546a0fe4431
GitHub Action <action@github.com>
2026-05-21 04:09:33 +0000

    Automated update

--- Casks/dnschanger-desktop-arm.rb
+++ Casks/dnschanger-desktop-arm.rb
@@ -1,11 +1,11 @@
 cask "dnschanger-desktop-arm" do
-  version "v2.3.5"
+  version "v2.3.6"
 
-  url "https://github.com/DnsChanger/dnsChanger-desktop/releases/download/v2.3.5/DNS-Changer-mac-arm64-2.3.5.dmg"
+  url "https://github.com/DnsChanger/dnsChanger-desktop/releases/download/v2.3.6/DNS-Changer-mac-arm64-2.3.6.dmg"
   name "dnsChanger-desktop-arm"
   desc "DNS Changer for Windows, Linux, Mac operating systems"
   homepage "https://github.com/DnsChanger/dnsChanger-desktop"
-  sha256 "71365bc7212517ab158304676343317a1c100ccb61cdca1cbbfca0186f99728b"
+  sha256 "5fc70890a9b75c0bd72e1c015bdd53a4c432f97aa85e0d5e8c11bc7af7dc8f01"
 
   auto_updates true
 
--- Casks/justhireme.rb
+++ Casks/justhireme.rb
@@ -1,11 +1,11 @@
 cask "justhireme" do
-  version "v1.0.16"
+  version "v1.0.23"
 
-  url "https://github.com/vasu-devs/JustHireMe/releases/download/v1.0.16/JustHireMe_1.0.16_aarch64.dmg"
+  url "https://github.com/vasu-devs/JustHireMe/releases/download/v1.0.23/JustHireMe_1.0.23_aarch64.dmg"
   name "JustHireMe"
   desc "JustHireMe - Local-first AI job intelligence workbench for scraping roles, ranking fit, and generating tailored application materials."
   homepage "https://github.com/vasu-devs/JustHireMe"
-  sha256 "f729d8e282cf1340b08b1c811a1a0cd0e140a06a7aeb893b817741f1d339b7b3"
+  sha256 "bb22af31235c30285831edacd1ba778200af0303f0feb8365a278c902dbe19f7"
 
   auto_updates true
 
