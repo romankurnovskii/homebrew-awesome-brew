c55de3f45eea0cb7abf532a3b22d6169cae7f6b5
GitHub Action <action@github.com>
2026-05-28 04:02:17 +0000

    Automated update

--- Casks/justhireme.rb
+++ Casks/justhireme.rb
@@ -1,11 +1,11 @@
 cask "justhireme" do
-  version "v1.0.38"
+  version "v1.0.41"
 
-  url "https://github.com/vasu-devs/JustHireMe/releases/download/v1.0.38/JustHireMe_1.0.38_aarch64.dmg"
+  url "https://github.com/vasu-devs/JustHireMe/releases/download/v1.0.41/JustHireMe_1.0.41_aarch64.dmg"
   name "JustHireMe"
   desc "JustHireMe - Local-first AI job intelligence workbench for scraping roles, ranking fit, and generating tailored application materials."
   homepage "https://github.com/vasu-devs/JustHireMe"
-  sha256 "e7836ffe8961c2d371c0bf918df0bd977bd681cb180b38dc1464578cd15345a7"
+  sha256 "30db898ecdbcf4e9027b3ab11598c95d90bfa93a89e6067832b19586ed6be6e8"
 
   auto_updates true
 
