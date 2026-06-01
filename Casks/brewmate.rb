741089bc050fb21b365beb06f4a80938bfd846bc
GitHub Action <action@github.com>
2026-06-01 04:29:35 +0000

    Automated update

--- Casks/brewmate.rb
+++ Casks/brewmate.rb
@@ -5,7 +5,7 @@ cask "brewmate" do
   name "BrewMate"
   desc "Homebrew GUI apps manager"
   homepage "https://github.com/romankurnovskii/BrewMate"
-  sha256 "a3dfb0be62c05d4cf722d7aab18a206f8e11b698f876b288eaee5fb329177381"
+  sha256 "f8cd687bfd62843fb968749b39de4dc39bd72e0d1d12d7e77ebc54bb07c16002"
 
   auto_updates true
 
