bbe2e9272620930de7f6b18d7741295fa7449e43
GitHub Action <action@github.com>
2026-06-01 14:56:20 +0000

    Automated update

--- Casks/brewmate.rb
+++ Casks/brewmate.rb
@@ -1,11 +1,11 @@
 cask "brewmate" do
-  version '1.0.22'
+  version "1.0.22"
 
-  url "https://github.com/romankurnovskii/BrewMate/releases/download/1.0.21/BrewMate-1.0.21-universal.dmg"
+  url "https://github.com/romankurnovskii/BrewMate/releases/download/1.0.22/BrewMate-1.0.22-universal.dmg"
   name "BrewMate"
   desc "Homebrew GUI apps manager"
   homepage "https://github.com/romankurnovskii/BrewMate"
-  sha256 'ea88d1851c0ecd16461b1429a33defec0361c360d9cfadaa81efd9c7fdb828d2'
+  sha256 "ba37f3315e5dd08e756ccb936125ef98bbb5628d6ca5c9207c7dd2d7fba94279"
 
   auto_updates true
 
