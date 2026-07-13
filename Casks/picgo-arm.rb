3026e48ed40e542bd8ae60cceb3b4337660f41c3
GitHub Action <action@github.com>
2026-07-13 03:28:47 +0000

    Automated update

--- Casks/picgo-arm.rb
+++ Casks/picgo-arm.rb
@@ -1,11 +1,11 @@
 cask "picgo-arm" do
-  version "v3.0.0"
+  version "v3.0.1"
 
-  url "https://github.com/Molunerfinn/PicGo/releases/download/v3.0.0/PicGo-3.0.0-arm64.dmg"
+  url "https://github.com/Molunerfinn/PicGo/releases/download/v3.0.1/PicGo-3.0.1-arm64.dmg"
   name "PicGo-arm"
   desc "A simple & beautiful tool for pictures uploading built by vue-cli-electron-builder"
   homepage "https://github.com/Molunerfinn/PicGo"
-  sha256 "11d30a6d1e43fdc6aa08ba69867056ce7e187907a86d53ca40d8564a3442dac6"
+  sha256 "f125eb158966616996e0715608044d4fc7cd8d36f85f877e5c8762ef550c5ae2"
 
   auto_updates true
 
--- Casks/picgo-intel.rb
+++ Casks/picgo-intel.rb
@@ -1,11 +1,11 @@
 cask "picgo-intel" do
-  version "v3.0.0"
+  version "v3.0.1"
 
-  url "https://github.com/Molunerfinn/PicGo/releases/download/v3.0.0/PicGo-3.0.0-x64.dmg"
+  url "https://github.com/Molunerfinn/PicGo/releases/download/v3.0.1/PicGo-3.0.1-x64.dmg"
   name "PicGo-intel"
   desc "A simple & beautiful tool for pictures uploading built by vue-cli-electron-builder"
   homepage "https://github.com/Molunerfinn/PicGo"
-  sha256 "e173ed0233735f9c95a3dbb33afd88bb522675bffecf4dba351d3bf0a2aed396"
+  sha256 "0a5430233833eaa59294b7ea39a47e8e206abf438738dd99dc01f237cc3f136f"
 
   auto_updates true
 
