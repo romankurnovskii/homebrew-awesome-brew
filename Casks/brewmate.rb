cf5ade2f4cf499e0ca460a1022a2488a65fcaad0
Roman <1992roman@list.ru>
2026-08-08 09:16:37 +0300

    update brewmate to 1.0.38

--- Casks/brewmate.rb
+++ Casks/brewmate.rb
@@ -1,11 +1,11 @@
 cask "brewmate" do
-  version '1.0.35'
+  version '1.0.38'
 
   url "https://github.com/romankurnovskii/BrewMate/releases/download/#{version}/BrewMate-#{version}-universal.dmg"
   name "BrewMate"
   desc "Homebrew GUI apps manager"
   homepage "https://github.com/romankurnovskii/BrewMate"
-  sha256 'fa71c04153f2c3b02a08792eff8996db03015826dd8c2219c3890992bb81e5ca'
+  sha256 'fb3200f79150d783f782d2f62a17e638d499ae3ef28e94da1ef04b8c7d37aef4'
 
   auto_updates true
 
