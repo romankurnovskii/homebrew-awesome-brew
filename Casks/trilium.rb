ef65956b4d1330d96e8a66799aa45a53eb97b839
GitHub Action <action@github.com>
2026-09-26 04:20:28 +0000

    Automated update

--- Casks/trilium.rb
+++ Casks/trilium.rb
@@ -1,11 +1,11 @@
 cask "trilium" do
-  version "v0.105.0"
+  version "v0.106.0"
 
-  url "https://github.com/TriliumNext/Trilium/releases/download/v0.105.0/TriliumNotes-v0.105.0-macos-arm64.zip"
+  url "https://github.com/TriliumNext/Trilium/releases/download/v0.106.0/TriliumNotes-v0.106.0-macos-arm64.zip"
   name "trilium"
   desc "Build your personal knowledge base with Trilium Notes"
   homepage "https://github.com/zadam/trilium"
-  sha256 "441901c820214580c109b1e6ec8e4e9651863e7e0e5401f882a7bf120db43c2b"
+  sha256 "4759da9aa1a1f6c7052fdb547d72ac4ac53946317dd09ea889583914b443f242"
 
   auto_updates true
 
